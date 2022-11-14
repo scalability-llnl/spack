a1bad26bdaf75197a9c577c14695581a9c6c5a9c
Harmen Stoppels <harmenstoppels@gmail.com>
2022-11-14 11:36:18 +0100

    strict mode

--- lib/spack/spack/schema/config.py
+++ lib/spack/spack/schema/config.py
@@ -34,6 +34,7 @@ properties: Dict[str, Any] = {
                         "properties": {
                             "type": {"type": "string", "enum": ["rpath", "runpath"]},
                             "bind": {"type": "boolean"},
+                            "strict": {"type": "boolean"},
                         },
                     },
                 ]
