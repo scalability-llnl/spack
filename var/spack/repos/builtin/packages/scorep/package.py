39d788dded98a5381e00002626f6c8efb93c6481
Bill Williams <wwilliam47@gmail.com>
2023-06-05 10:32:02 +0200

    We need shared-only binutils
    Apparently static binutils doesn't get built with -fPIC in Spack, and somehow our configure tests try to link against non-PIC static binutils in preference to PIC shared binutils when both are present.

--- var/spack/repos/builtin/packages/scorep/package.py
+++ var/spack/repos/builtin/packages/scorep/package.py
@@ -98,7 +98,7 @@ class Scorep(AutotoolsPackage):
     # two components of cube -- cubew and cubelib.
 
     # SCOREP 8
-    depends_on("binutils", type="link", when="@8:")
+    depends_on("binutils+libs=shared~libs=static", type="link", when="@8:")
     depends_on("otf2@3:", when="@8:")
     depends_on("cubew@4.8.2:", when="@8.3:")
     depends_on("cubelib@4.8.2:", when="@8.3:")
@@ -172,6 +172,7 @@ class Scorep(AutotoolsPackage):
             "--with-otf2=%s" % spec["otf2"].prefix.bin,
             "--with-opari2=%s" % spec["opari2"].prefix.bin,
             "--enable-shared",
+            "--disable-static"
         ]
 
         cname = self.clean_compiler(spec.compiler.name)
