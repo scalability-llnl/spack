41084a7b179d66960a252b0658174e9ec0d0943b
Benjamin Kirk <benjamin.s.kirk@gmail.com>
2024-02-23 17:58:09 +0000

    pmix: autotools + perl required to bootstrap newer releases, not just @master

--- var/spack/repos/builtin/packages/pmix/package.py
+++ var/spack/repos/builtin/packages/pmix/package.py
@@ -85,11 +85,11 @@ class Pmix(AutotoolsPackage):
 
     variant("docs", default=False, description="Build manpages")
 
-    depends_on("m4", type="build", when="@master")
-    depends_on("autoconf", type="build", when="@master")
-    depends_on("automake", type="build", when="@master")
-    depends_on("libtool", type="build", when="@master")
-    depends_on("perl", type="build", when="@master")
+    depends_on("m4", type="build")
+    depends_on("autoconf", type="build")
+    depends_on("automake", type="build")
+    depends_on("libtool", type="build")
+    depends_on("perl", type="build")
     depends_on("pandoc", type="build", when="+docs")
     depends_on("pkgconfig", type="build")
 
