fec2b85f3443f55437a6c924bd29ac4800a4f0b9
Benjamin Kirk <benjamin.s.kirk@gmail.com>
2024-02-27 16:17:38 +0000

    libwmf: need #include <limits.h>

--- var/spack/repos/builtin/packages/libwmf/package.py
+++ var/spack/repos/builtin/packages/libwmf/package.py
@@ -19,6 +19,7 @@ class Libwmf(AutotoolsPackage):
 
     license("LGPL-2.0-or-later")
 
+    version("0.2.13", sha256="18ba69febd2f515d98a2352de284a8051896062ac9728d2ead07bc39ea75a068")
     version("0.2.12", sha256="464ff63605d7eaf61a4a12dbd420f7a41a4d854675d8caf37729f5bc744820e2")
     version("0.2.11", sha256="e2a2664afd5abc71a42be7ad3c200f64de2b8889bf088eac1d32e205ce843803")
 
@@ -31,6 +32,8 @@ class Libwmf(AutotoolsPackage):
     depends_on("libpng")
     depends_on("libjpeg")
     depends_on("zlib-api")
+
+    patch("limits_h.patch", sha256="9e8a6fe9c21f1db86409bca2e82f5c2df0cc4ffc250abc0db6e2d1b3535c3315")
 
     def configure_args(self):
         args = ["--disable-static"]
