498242149ff98f61038cdb778a36bc8d77b5f14a
Benjamin Kirk <benjamin.s.kirk@gmail.com>
2024-02-27 19:56:21 +0000

    mpfr: requires perl to bootstrap

--- var/spack/repos/builtin/packages/mpfr/package.py
+++ var/spack/repos/builtin/packages/mpfr/package.py
@@ -39,6 +39,7 @@ class Mpfr(AutotoolsPackage, GNUMirrorPackage):
     depends_on("libtool", type="build")
     depends_on("m4", type="build")
     depends_on("autoconf-archive", when="@4.0.0:", type="build")
+    depends_on("perl", type="build")
     depends_on("texinfo", when="@4.1.0:", type="build")
 
     variant(
