ee014f881e6ec164d84c834547fdcbbbace74105
Darby Vicker <darby.vicker-1@nasa.gov>
2024-03-13 14:21:52 -0500

    Update tecio for the FSL

--- var/spack/repos/builtin/packages/tecio/package.py
+++ var/spack/repos/builtin/packages/tecio/package.py
@@ -13,17 +13,20 @@ class Tecio(CMakePackage):
     Tecplot file format."""
 
     homepage = "https://www.tecplot.com/products/tecio-library/"
-    url = "file://{}/tecio.tgz".format(os.getcwd())
+    #url = "file://{}/tecio.tgz".format(os.getcwd())
+    url = "file:///aerolab/admin/software/dist/tecplot/tecio.2023.1.1.tgz"
 
     manual_download = True
 
     maintainers("snehring")
 
-    version("2022R2", sha256="35a8137800611da3a413da09223e2d1683b54e195bc52f8fc4ec131cfce7cb23")
+    version("2023.1.1", sha256="1468430e3cf6f019175da2559e63c5d6cf61c8569a3e994890448d1c6f7aa1f3")
 
-    depends_on("cmake@3.0.2:", type="build")
 
-    depends_on("boost@1.69.0:+system", type="build")
+    #depends_on("cmake@3.0.2:", type="build")
+
+    #depends_on("boost@1.69.0:+system", type="build")
+    depends_on("boost", type="build")
     depends_on("mpi", when="+mpi")
 
     variant("mpi", default=False, description="Build tecio with mpi support.")
