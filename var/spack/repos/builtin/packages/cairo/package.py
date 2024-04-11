384afd559bee143f611203b22f513b90553070c1
Wouter Deconinck <wdconinc@gmail.com>
2024-04-10 20:05:00 -0500

    cairo: "new" version 1.18.0 (now also MesonPackage)

--- var/spack/repos/builtin/packages/cairo/package.py
+++ var/spack/repos/builtin/packages/cairo/package.py
@@ -5,14 +5,21 @@
 from spack.package import *
 
 
-class Cairo(AutotoolsPackage):
+class Cairo(MesonPackage, AutotoolsPackage):
     """Cairo is a 2D graphics library with support for multiple output
     devices."""
 
     homepage = "https://www.cairographics.org/"
     url = "https://www.cairographics.org/releases/cairo-1.16.0.tar.xz"
+    git = "https://gitlab.freedesktop.org/cairo/cairo.git"
 
     license("LGPL-2.1-or-later OR MPL-1.1", checked_by="tgamblin")
+
+    build_system(
+        conditional("autotools", when="@:1.17.6"),
+        conditional("meson", when="@1.17.8:"),
+        default="meson",
+    )
 
     version("1.18.0", sha256="243a0736b978a33dee29f9cca7521733b78a65b5418206fef7bd1c3d4cf10b64")
     version(
@@ -28,7 +35,6 @@ class Cairo(AutotoolsPackage):
     version(
         "1.16.0",
         sha256="5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331",
-        preferred=True,
     )
     version("1.14.12", sha256="8c90f00c500b2299c0a323dd9beead2a00353752b2092ead558139bd67f7bf16")
     version("1.14.8", sha256="d1f2d98ae9a4111564f6de4e013d639cf77155baf2556582295a0f00a9bc5e20")
@@ -47,6 +53,15 @@ class Cairo(AutotoolsPackage):
     variant("shared", default=True, description="Build shared libraries")
     variant("pic", default=True, description="Enable position-independent code (PIC)")
 
+    with when("build_system=autotools"):
+        depends_on("autoconf", type="build")
+        depends_on("automake", type="build")
+        depends_on("libtool", type="build")
+        depends_on("m4", type="build")
+
+    with when("build_system=meson"):
+        depends_on("meson@0.59:")
+
     depends_on("libx11", when="+X")
     depends_on("libxext", when="+X")
     depends_on("libxrender", when="+X")
@@ -56,14 +71,11 @@ class Cairo(AutotoolsPackage):
     depends_on("glib")
     depends_on("pixman@0.36.0:", when="@1.17.2:")
     depends_on("pixman")
-    depends_on("automake", type="build")
-    depends_on("autoconf", type="build")
-    depends_on("libtool", type="build")
-    depends_on("m4", type="build")
     depends_on("freetype build_system=autotools", when="+ft")
     depends_on("pkgconfig", type="build")
     depends_on("fontconfig@2.10.91:", when="+fc")  # Require newer version of fontconfig.
     depends_on("which", type="build")
+    depends_on("zlib", when="+pdf")
 
     conflicts("+png", when="platform=darwin")
     conflicts("+svg", when="platform=darwin")
@@ -72,9 +84,33 @@ class Cairo(AutotoolsPackage):
     # patch from https://gitlab.freedesktop.org/cairo/cairo/issues/346
     patch("fontconfig.patch", when="@1.16.0:1.17.2")
     # Don't regenerate docs to avoid a dependency on gtk-doc
-    patch("disable-gtk-docs.patch", when="^autoconf@2.70:")
+    patch("disable-gtk-docs.patch", when="build_system=autotools ^autoconf@2.70:")
 
-    def autoreconf(self, spec, prefix):
+    def check(self):
+        """The checks are only for the cairo devs: They write others shouldn't bother"""
+        pass
+
+class MesonBuilder(spack.build_systems.meson.MesonBuilder):
+    def meson_args(self):
+        args = ["-Dtee=enabled"]
+
+        if "+X" in self.spec:
+            args.extend(["-Dxlib=enabled", "-Dxcb=enabled"])
+        else:
+            args.extend(["-Dxlib=disabled", "-Dxcb=disabled"])
+
+        args.append("-Dzlib=" + ("enabled" if "pdf" in self.spec else "disabled"))
+        args.append("-Dpng=" + ("enabled" if ("png" in self.spec or "svg" in self.spec) else "disabled"))
+        args.append("-Dfreetype=" + ("enabled" if "ft" in self.spec else "disabled"))
+        args.append("-Dfontconfig=" + ("enabled" if "fc" in self.spec else "disabled"))
+        args.append("-Ddefault_library=" + ("shared" if "shared" in self.spec else "static"))
+        args.append("-Db_staticpic=" + ("true" if "pic" in self.spec else "false"))
+
+        return args
+
+
+class AutotoolsBuilder(spack.build_systems.autotools.AutotoolsBuilder):
+    def autoreconf(self, pkg, spec, prefix):
         # Regenerate, directing the script *not* to call configure before Spack
         # does
         which("sh")("./autogen.sh", extra_env={"NOCONFIGURE": "1"})
@@ -102,7 +138,3 @@ class Cairo(AutotoolsPackage):
             args.append(f"LIBS={libs}")
 
         return args
-
-    def check(self):
-        """The checks are only for the cairo devs: They write others shouldn't bother"""
-        pass
