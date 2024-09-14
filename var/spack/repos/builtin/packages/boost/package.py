951b61654b0ed921473fb6f46ea2c2d9436726c2
Tim Haines <thaines.astro@gmail.com>
2024-09-14 18:55:24 -0500

    Extract filesystem variant

--- var/spack/repos/builtin/packages/boost/package.py
+++ var/spack/repos/builtin/packages/boost/package.py
@@ -89,7 +89,6 @@ class Boost(Package):
 
     with_default_variants = "boost" + "".join(
         [
-            "+filesystem",
             "+graph",
             "+locale",
             "+log",
@@ -110,7 +109,6 @@ class Boost(Package):
     # dependencies and/or because there is a great deal of customization
     # possible (and it would be difficult to choose sensible defaults)
     all_libs = [
-        "filesystem",
         "graph",
         "graph_parallel",
         "json",
