11d147b63b265aeaa8a919b7681a0d21f2e919f1
Tim Haines <thaines.astro@gmail.com>
2024-09-14 17:29:24 -0500

    Extract, cleanup mpi variant
    This slightly changes the logic of the conflict with 1.64.0 when
    +python is enabled, but is now consistent with the comment. That is,
    the conflict happens with @1.64.0+python+mpi instead of just
    @1.64.0+python.

--- var/spack/repos/builtin/packages/boost/boostorg/variants.py
+++ var/spack/repos/builtin/packages/boost/boostorg/variants.py
@@ -206,6 +206,24 @@ def load():
         description="C++ wrapper for interacting with Python.",
     )
     _boost_variant(
+        "mpi",
+        default=False,
+        sticky=False,
+        when="@1.35.0:",
+        buildable="@1.35.0:",
+        conflicts=[
+            # 1.64 uses out-dated APIs (https://github.com/spack/spack/issues/3963)
+            {"when": "@1.64.0 +python", "msg": "Boost.MPI@1.64.0 does not support python"},
+            {"when": "@1.72.0 cxxstd=98", "msg": "Boost.MPI@1.72.0 does not support C++98"},
+        ],
+        requires=[
+            {"spec": "+python", "when": "@1.87.0:", "msg": "Boost.MPI requires Boost.Numpy"}
+        ],
+        description=(
+            "C++ wrapper to the Message Passing Interface for distributed-memory parallelism."
+        ),
+    )
+    _boost_variant(
         "container",
         # Can be both header-only and compiled. '+container' indicates the
         # compiled version which requires Extended Allocator support. The
