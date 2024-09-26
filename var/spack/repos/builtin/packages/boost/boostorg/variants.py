cde4ea333c37b16dac2b95626ee0aa64eea77af4
Tim Haines <thaines.astro@gmail.com>
2024-09-26 14:17:31 -0500

    Add updates from 1.81.0 release notes

--- var/spack/repos/builtin/packages/boost/boostorg/variants.py
+++ var/spack/repos/builtin/packages/boost/boostorg/variants.py
@@ -702,6 +702,10 @@ def load():
         default=False,
         when="@1.48.0:",
         buildable="@1.48.0:",
+        conflicts=[
+            {"when": "@1.81.0: cxxstd=98", "msg": "Boost.Locale requires cxxstd >= 11"},
+            {"when": "@1.81.0: cxxstd=03", "msg": "Boost.Locale requires cxxstd >= 11"},
+        ],
         requires=[
             {"spec": "+icu", "msg": "Boost.Locale requires Unicode support"}
         ],
