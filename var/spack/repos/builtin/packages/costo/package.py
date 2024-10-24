4b49ec106c37637b53ff35d727bb10fcf74ab7ba
t. chantrait <teddy.chantrait@cea.fr>
2024-10-24 10:06:59 +0200

    [costo]: fix related to discussion #47121

--- var/spack/repos/builtin/packages/costo/package.py
+++ var/spack/repos/builtin/packages/costo/package.py
@@ -7,7 +7,9 @@ from spack.package import *
 
 
 class Costo(CMakePackage):
-    """Cosimulation Tools"""
+    """costo stand for COSimulation TOols.
+    Its a layer above MPI to share data between meshes.
+    """
 
     homepage = "https://gitlab.com/Te_ch/costo"
     git = "https://gitlab.com/Te_ch/costo.git"
@@ -16,25 +18,23 @@ class Costo(CMakePackage):
 
     license("LGPL-3.0-or-later")
 
-    version(
-        "0.0.4", tag="v0.0.4", commit="f9e3ee774a33cc3af566e338d542d7dd0ff36cea", preferred=True
-    )
+    version("0.0.5", tag="v0.0.5", preferred=True)
     version("develop", branch="devel")
-    version("master", branch="main", deprecated=True)
+    version("main", branch="main", deprecated=True)
 
     variant("shared", default=True, description="Build shared library")
     variant("tests", default=False, description="Enable testing")
 
     depends_on("mpi", type=all)
-    depends_on("python@3.0:", type=all)
+    depends_on("python@3.10:", type=all)
 
     depends_on("py-non-regression-test-tools", type="build")
-    depends_on("py-pyvista", type="run")
+    depends_on("py-pyvista", type=("build", "run"))
     depends_on("py-numpy", type=("build", "link", "run"))
-    depends_on("py-mpi4py", type="run")
-    depends_on("py-scipy", type="run")
-    depends_on("py-mgmetis", type="run")
-    depends_on("py-colorama", type="run")
+    depends_on("py-mpi4py", type=("build", "run"))
+    depends_on("py-scipy", type=("build", "run"))
+    depends_on("py-mgmetis", type=("build", "run"))
+    depends_on("py-colorama", type=("build", "run"))
     depends_on("py-pip", type="build")
 
     def cmake_args(self):
