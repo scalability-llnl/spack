281ff24237520dfa4823339412782d33a577f7e1
Gregor Olenik <gregor.olenik@web.de>
2024-10-25 15:24:10 +0200

    add neofoam package.py

---
+++ var/spack/repos/builtin/packages/neofoam/package.py
@@ -0,0 +1,21 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: MIT
+
+from spack.package import *
+
+class Neofoam(CMakePackage):
+    """NeoFOAM is a WIP prototype of a modern CFD core."""
+
+    homepage = "https://github.com/exasim-project/NeoFOAM"
+    git = homepage
+
+    # maintainers("greole", "github_user2")
+
+    license("MIT", checked_by="greole")
+
+    version("develop", branch="main")
+
+    depends_on("mpi")
+    depends_on("kokkos@4.3.0")
