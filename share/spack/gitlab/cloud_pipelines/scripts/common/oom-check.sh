bd117d6b0fef416deee51516176c1302736a3172
caetano melone <caetanomelone@gmail.com>
2024-10-29 19:00:15 -0300

    Update `oom-check.sh`
    Add clarifying comment about the isolation of the check wrt the node and write to generic `job_metadata.txt` file

--- share/spack/gitlab/cloud_pipelines/scripts/common/oom-check.sh
+++ share/spack/gitlab/cloud_pipelines/scripts/common/oom-check.sh
@@ -11,9 +11,6 @@ set -e
 # https://gitlab.com/outschool-eng/gitlab-runner/-/commit/65d5c4d468ffdbde0ceeafd9168d1326bae8e708
 # we rely upon the ability to view kernel messages in the build container
 
-SPACK_ARTIFACTS_ROOT=${CI_PROJECT_DIR}/jobs_scratch_dir
-mkdir -p ${SPACK_ARTIFACTS_ROOT}/user_data
-
 # exit early if job was successful or not on AWS
 [[ "$CI_JOB_STATUS" != "failed" ]] && exit 0
 [[ "$CI_RUNNER_TAGS" != *"aws"* ]] && exit 0
@@ -24,23 +21,29 @@ if [[ ! -f /proc/1/cgroup ]]; then
   exit 1
 fi
 
-OOM_MESSAGE="This job was killed due to memory constraints. Report to #ci-and-pipelines in Slack if you need help."
-
 # Look for an OOM kill in the build container cpuset or any other container with the pod level memory in path
 # the dmesg line will look like:
 # [ 1578.430541] oom-kill:constraint=CONSTRAINT_MEMCG,nodemask=(null),cpuset=657a5777a8dbad52481bde927e9464ce5a838ad75f14ddf4322a32104786bce2,mems_allowed=0,oom_memcg=/kubepods/burstable/pod53bff6f9-f52d-418b-abf1-b5df128eb9cd/657a5777a8dbad52481bde927e9464ce5a838ad75f14ddf4322a32104786bce2,task_memcg=/kubepods/burstable/pod53bff6f9-f52d-418b-abf1-b5df128eb9cd/657a5777a8dbad52481bde927e9464ce5a838ad75f14ddf4322a32104786bce2,task=sh,pid=30361,uid=0
 # where the last chunk of cgroup would be 657a5777a8dbad52481bde927e9464ce5a838ad75f14ddf4322a32104786bce2
 # and the pod level memory config dir would be pod53bff6f9-f52d-418b-abf1-b5df128eb9cd, sourced from the second to last chunk
+# we are not interfering with other processes on the same node, as the runner is in a container
+# and we are filtering by the pod/container-level cgroup identifier in the dmesg output
 
 proc1_cgroup=$(cat /proc/1/cgroup)
 ctr_cgroup=$(echo "$proc1_cgroup" | tr / '\n' | tail -1 | tr -d '[:space:]')
 pod_cgroup=$(echo "$proc1_cgroup" | tr / '\n' | tail -2 | head -1 | tr -d '[:space:]')
 dmesg_out=$(dmesg)
 
+OOM_MESSAGE="This job was killed due to memory constraints. Report to #ci-and-pipelines in Slack if you need help."
+SPACK_ARTIFACTS_ROOT=${CI_PROJECT_DIR}/jobs_scratch_dir
+JOB_INFO="${SPACK_ARTIFACTS_ROOT}/logs/job_metadata.txt"
+
+mkdir -p ${SPACK_ARTIFACTS_ROOT}/logs
+
 if echo "$dmesg_out" | grep -q "oom-kill.*$ctr_cgroup"; then
   echo $OOM_MESSAGE
-  echo "OOM info: container" > ${SPACK_ARTIFACTS_ROOT}/user_data/oom-info
+  echo "OOM: container" >> $JOB_INFO
 elif echo "$dmesg_out" | grep -q "oom-kill.*$pod_cgroup"; then
   echo $OOM_MESSAGE
-  echo "OOM info: pod" > ${SPACK_ARTIFACTS_ROOT}/user_data/oom-info
+  echo "OOM: pod" >> $JOB_INFO
 fi
