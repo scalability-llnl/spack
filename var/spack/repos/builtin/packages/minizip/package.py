7851a4a92148c2f8ce97c9ca125e9b6763ce8b35
Wouter Deconinck <wdconinc@gmail.com>
2024-11-02 12:37:02 -0500

    minizip: fix style

--- var/spack/repos/builtin/packages/minizip/package.py
+++ var/spack/repos/builtin/packages/minizip/package.py
@@ -17,7 +17,9 @@ class Minizip(AutotoolsPackage):
     version("1.3.1", sha256="9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23")
     with default_args(deprecated=True):
         # https://nvd.nist.gov/vuln/detail/CVE-2022-37434
-        version("1.2.11", sha256="c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1")
+        version(
+            "1.2.11", sha256="c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"
+        )
 
     depends_on("c", type="build")
 
