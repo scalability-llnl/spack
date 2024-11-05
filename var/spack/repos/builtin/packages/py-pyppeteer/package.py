87d5e6655f4e18149f3a828b72694ce1d1340e3e
Wouter Deconinck <wdconinc@gmail.com>
2024-11-05 11:48:47 -0600

    py-pyppeteer: add dependencies

--- var/spack/repos/builtin/packages/py-pyppeteer/package.py
+++ var/spack/repos/builtin/packages/py-pyppeteer/package.py
@@ -18,3 +18,12 @@ class PyPyppeteer(PythonPackage):
     version("2.0.0", sha256="4af63473ff36a746a53347b2336a49efda669bcd781e400bc1799b81838358d9")
 
     depends_on("py-poetry-core", type="build")
+
+    depends_on("python@3.8:", type=("build", "run")
+    depends_on("py-appdirs@1.4.3:1", type=("build", "run")
+    depends_on("py-import-metadata@1.4:", type=("build", "run")
+    depends_on("py-pyee@11", type=("build", "run")
+    depends_on("py-tqdm@4.42.1:4", type=("build", "run")
+    depends_on("py-urllib3@1.25.8:1", type=("build", "run")
+    depends_on("py-websockets@10", type=("build", "run")
+    depends_on("py-certifi@2023:", type=("build", "run")
