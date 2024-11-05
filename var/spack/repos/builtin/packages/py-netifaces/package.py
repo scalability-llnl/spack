097f89cbba83dfef9e89883259fcea1233e3d996
Wouter Deconinck <wdconinc@gmail.com>
2024-11-05 14:59:24 -0600

    py-netifaces: fix style

--- var/spack/repos/builtin/packages/py-netifaces/package.py
+++ var/spack/repos/builtin/packages/py-netifaces/package.py
@@ -14,7 +14,7 @@ class PyNetifaces(PythonPackage):
 
     license("MIT", checked_by="wdconinc")
 
-    version("0.11.0", sha256="043a79146eb2907edf439899f262b3dfe41717d34124298ed281139a8b93ca32") 
+    version("0.11.0", sha256="043a79146eb2907edf439899f262b3dfe41717d34124298ed281139a8b93ca32")
     version("0.10.9", sha256="2dee9ffdd16292878336a58d04a20f0ffe95555465fee7c9bd23b3490ef2abf3")
     version("0.10.5", sha256="59d8ad52dd3116fcb6635e175751b250dc783fb011adba539558bd764e5d628b")
 
