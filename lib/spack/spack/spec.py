e8fc64f38dacb4edc211898458ef6926a4a48ed3
Harmen Stoppels <harmenstoppels@gmail.com>
2024-11-07 18:11:10 +0100

    Revert "Specs: propagated variants affect `==` equality (#47376)"
    This reverts commit 395c911689d7a3ee426660f3fab334ccee04da1a.

--- lib/spack/spack/spec.py
+++ lib/spack/spack/spec.py
@@ -877,9 +877,8 @@ class FlagMap(lang.HashableMap):
                 # Next, if any flags in other propagate, we force them to propagate in our case
                 shared = list(sorted(set(other[flag_type]) - extra_other))
                 for x, y in _shared_subset_pair_iterate(shared, sorted(self[flag_type])):
-                    if y.propagate is True and x.propagate is False:
-                        changed = True
-                        y.propagate = False
+                    if x.propagate:
+                        y.propagate = True
 
         # TODO: what happens if flag groups with a partial (but not complete)
         # intersection specify different behaviors for flag propagation?
@@ -934,7 +933,6 @@ class FlagMap(lang.HashableMap):
             def flags():
                 for flag in v:
                     yield flag
-                    yield flag.propagate
 
             yield flags
 
