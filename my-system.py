6daeb5c6771dfe30c12be107863d342bb5d06291
Peter Scheibel <scheibel1@llnl.gov>
2024-11-07 17:17:14 -0800

    necessary arch

--- my-system.py
+++ my-system.py
@@ -4,13 +4,17 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 import spack.environment as ev
 import spack.config as config
+import spack.platforms
 from spack.spec import Spec
 import spack.util.spack_yaml as syaml
 
 from llnl.util.filesystem import mkdirp
 
+import archspec.cpu
+
 import argparse
 import pathlib
+import pickle
 import os
 
 
@@ -19,6 +23,13 @@ def _dump_section(section, stream):
     data[section] = config.CONFIG.get_config(section, scope=None)
     syaml.dump_config(data, stream=stream, default_flow_style=False, blame=None)
 
+def _system_pickles(dst):
+    data = [
+        spack.platforms.host(),
+        archspec.cpu.host()
+    ]
+    with open(dst, "wb") as f:
+        pickle.dump(data, f)
 
 def main():
     parser = argparse.ArgumentParser(description="Collect details for simulating this system elsewhere")
@@ -28,12 +39,17 @@ def main():
     if os.path.exists(args.dest):
         raise ValueError(f"Dest already exists: {args.dest}")
 
-    cfg = pathlib.Path(args.dest) / "config"
+    root = pathlib.Path(args.dest)
+    cfg = root / "config"
     mkdirp(cfg)
     with open(cfg / "packages.yaml", "w") as f:
         _dump_section("packages", f)
     with open(cfg / "compilers.yaml", "w") as f:
         _dump_section("compilers", f)
+    with open(cfg / "concretizer.yaml", "w") as f:
+        _dump_section("concretizer", f)
+
+    _system_pickles(root / "arch.pkl")
 
 
 if __name__ == "__main__":
