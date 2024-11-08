68c62b8e98743427cf1a771e6af9f0a8175929bd
wdconinc <wdconinc@users.noreply.github.com>
2024-11-08 22:18:21 +0000

    [@spackbot] updating style on behalf of wdconinc

--- var/spack/repos/builtin/packages/py-flask/package.py
+++ var/spack/repos/builtin/packages/py-flask/package.py
@@ -23,10 +23,18 @@ class PyFlask(PythonPackage):
         version("2.0.2", sha256="7b2fb8e934ddd50731893bdcdb00fc8c0315916f9fcd50d22c7cc1a95ab634e2")
         version("1.1.2", sha256="4efa1ae2d7c9865af48986de8aeb8504bf32c7f3d6fdc9353d34b21f4b127060")
         version("1.1.1", sha256="13f9f196f330c7c2c5d7a5cf91af894110ca0215ac051b5844701f2bfd934d52")
-        version("0.12.4", sha256="2ea22336f6d388b4b242bc3abf8a01244a8aa3e236e7407469ef78c16ba355dd")
-        version("0.12.2", sha256="49f44461237b69ecd901cc7ce66feea0319b9158743dd27a2899962ab214dac1")
-        version("0.12.1", sha256="9dce4b6bfbb5b062181d3f7da8f727ff70c1156cbb4024351eafd426deb5fb88")
-        version("0.11.1", sha256="b4713f2bfb9ebc2966b8a49903ae0d3984781d5c878591cf2f7b484d28756b0e")
+        version(
+            "0.12.4", sha256="2ea22336f6d388b4b242bc3abf8a01244a8aa3e236e7407469ef78c16ba355dd"
+        )
+        version(
+            "0.12.2", sha256="49f44461237b69ecd901cc7ce66feea0319b9158743dd27a2899962ab214dac1"
+        )
+        version(
+            "0.12.1", sha256="9dce4b6bfbb5b062181d3f7da8f727ff70c1156cbb4024351eafd426deb5fb88"
+        )
+        version(
+            "0.11.1", sha256="b4713f2bfb9ebc2966b8a49903ae0d3984781d5c878591cf2f7b484d28756b0e"
+        )
 
     depends_on("python@3.8:", when="@2.3:", type=("build", "run"))
     depends_on("py-setuptools", type=("build", "run"), when="@:2")
