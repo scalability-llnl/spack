c367bfc8aa943b090c48f5f88235a5231063719d
Wouter Deconinck <wdconinc@gmail.com>
2024-11-08 13:28:32 -0600

    py-starlette: add v0.37.2, v0.41.2

--- var/spack/repos/builtin/packages/py-starlette/package.py
+++ var/spack/repos/builtin/packages/py-starlette/package.py
@@ -15,6 +15,8 @@ class PyStarlette(PythonPackage):
 
     license("BSD-3-Clause")
 
+    version("0.41.2", sha256="9834fd799d1a87fd346deb76158668cfa0b0d56f85caefe8268e2d97c3468b62")
+    version("0.37.2", sha256="9af890290133b79fc3db55474ade20f6220a364a0402e0b556e7cd5e1e093823")
     version("0.27.0", sha256="6a6b0d042acb8d469a01eba54e9cda6cbd24ac602c4cd016723117d6a7e73b75")
     version("0.23.1", sha256="8510e5b3d670326326c5c1d4cb657cc66832193fe5d5b7015a51c7b1e1b1bf42")
     version("0.22.0", sha256="b092cbc365bea34dd6840b42861bdabb2f507f8671e642e8272d2442e08ea4ff")
