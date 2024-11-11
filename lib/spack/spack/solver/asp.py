aaa7677cb9c835aaa6b0a8a660e0cd2d9c02a383
GouwarPower <jgouwar@gmail.com>
2024-11-11 08:18:28 -0500

    Removes vestigial timer

--- lib/spack/spack/solver/asp.py
+++ lib/spack/spack/solver/asp.py
@@ -889,7 +889,6 @@ class PyclingoDriver:
         timer.start("solve")
         solve_result = self.control.solve(**solve_kwargs)
         timer.stop("solve")
-        timer.stop()
 
         # once done, construct the solve result
         result = Result(specs)
