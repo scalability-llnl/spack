3ffe7b3fe168ee5b5c56371e39c7eed3dc7a7b65
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-11-11 12:48:50 +0100

    solver: first working implementation of compiler as nodes
    This commit changes the model to treat compilers as nodes, and
    drops the concept of a "compiler" as a bundle of a C, C++, and
    Fortran compiler.
    
    Implementation does not rely on `Compiler` or `CompilerSpec`.

--- lib/spack/spack/solver/input_analysis.py
+++ lib/spack/spack/solver/input_analysis.py
@@ -461,7 +461,10 @@ class MinimalDuplicatesCounter(NoDuplicatesCounter):
         self._possible_dependencies = set(self._link_run) | set(self._total_build)
 
     def possible_packages_facts(self, gen, fn):
-        build_tools = spack.repo.PATH.packages_with_tags("build-tools")
+        build_tools = set()
+        for current_tag in ("build-tools", "compiler"):
+            build_tools.update(spack.repo.PATH.packages_with_tags(current_tag))
+
         gen.h2("Packages with at most a single node")
         for package_name in sorted(self.possible_dependencies() - build_tools):
             gen.fact(fn.max_dupes(package_name, 1))
@@ -474,8 +477,11 @@ class MinimalDuplicatesCounter(NoDuplicatesCounter):
         gen.newline()
 
         gen.h2("Maximum number of nodes (virtual packages)")
-        for package_name in sorted(self.possible_virtuals()):
+        for package_name in sorted(self._link_run_virtuals):
             gen.fact(fn.max_dupes(package_name, 1))
+        gen.newline()
+        for package_name in sorted(self.possible_virtuals() - self._link_run_virtuals):
+            gen.fact(fn.max_dupes(package_name, 2))
         gen.newline()
 
         gen.h2("Possible package in link-run subDAG")
@@ -486,7 +492,6 @@ class MinimalDuplicatesCounter(NoDuplicatesCounter):
 
 class FullDuplicatesCounter(MinimalDuplicatesCounter):
     def possible_packages_facts(self, gen, fn):
-        build_tools = spack.repo.PATH.packages_with_tags("build-tools")
         counter = collections.Counter(
             list(self._link_run) + list(self._total_build) + list(self._direct_build)
         )
@@ -497,6 +502,10 @@ class FullDuplicatesCounter(MinimalDuplicatesCounter):
         gen.newline()
 
         gen.h2("Build unification sets ")
+        build_tools = set()
+        for current_tag in ("build-tools", "compiler"):
+            build_tools.update(spack.repo.PATH.packages_with_tags(current_tag))
+
         for name in sorted(self.possible_dependencies() & build_tools):
             gen.fact(fn.multiple_unification_sets(name))
         gen.newline()
