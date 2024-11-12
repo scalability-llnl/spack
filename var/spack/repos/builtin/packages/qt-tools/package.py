c4f5a73460bea912eaa756c81e1d4e00d0849c86
Wouter Deconinck <wdconinc@gmail.com>
2024-11-12 15:38:36 -0600

    qt-tools: add variant assistant; use commits with submodule

--- var/spack/repos/builtin/packages/qt-tools/package.py
+++ var/spack/repos/builtin/packages/qt-tools/package.py
@@ -12,19 +12,29 @@ class QtTools(QtPackage):
     """Qt Tools contains tools like Qt Designer."""
 
     url = QtPackage.get_url(__qualname__)
+    git = QtPackage.get_git(__qualname__)
     list_url = QtPackage.get_list_url(__qualname__)
 
     maintainers("wdconinc")
 
     license("BSD-3-Clause")
 
-    version("6.7.2", sha256="3ae2db630606edf94cc368691ee1da9c0bae7a06ff46c544c459cece8b60b62a")
+    # src/assistant/qlitehtml is a submodule that is not in the git archive
+    version("6.7.3", commit="ec4747e62a837a0262212a5f4fb03734660c7360", submodules=True)
+    version("6.7.2", commit="46ffaed90df8c14d67b4b16fdf5e0b87ab227c88", submodules=True)
 
+    variant(
+        "assistant",
+        default=False,
+        description="Qt Assistant for viewing on-line documentation in Qt help file format.",
+    )
     variant(
         "designer",
         default=False,
         description="Qt Widgets Designer for designing and building GUIs with Qt Widgets.",
     )
+
+    depends_on("llvm +clang")
 
     depends_on("qt-base +network")
     depends_on("qt-base +widgets", when="+designer")
@@ -40,6 +50,9 @@ class QtTools(QtPackage):
 
         def define(cmake_var, value):
             args.append(self.define(cmake_var, value))
+
+        if spec.satisfies("+assistant"):
+            define("FEATURE_assistant", True)
 
         if spec.satisfies("+designer"):
             define("FEATURE_designer", True)
