bb0ec3d148c1cfd023b74708931d9242101cf89d
Matthieu Dorier <matthieu.dorier@gmail.com>
2024-11-12 09:18:38 +0000

    py-constantly: updated py-versioneer dependency
    Co-authored-by: Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>

--- var/spack/repos/builtin/packages/py-constantly/package.py
+++ var/spack/repos/builtin/packages/py-constantly/package.py
@@ -18,4 +18,4 @@ class PyConstantly(PythonPackage):
     version("15.1.0", sha256="586372eb92059873e29eba4f9dec8381541b4d3834660707faf8ba59146dfc35")
 
     depends_on("py-setuptools", type="build")
-    depends_on("py-versioneer@0.29", type="build", when="@23.10.4:")
+    depends_on("py-versioneer+toml@0.29", type="build", when="@23.10.4:")
