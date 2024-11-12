5493e97cdb541cf59db85ad84d6f4fd86fda1db1
Hubertus van Dam <hubertus.vandam@uni-due.de>
2024-11-12 13:39:16 +0100

    Style fixes

--- var/spack/repos/builtin/packages/pace/package.py
+++ var/spack/repos/builtin/packages/pace/package.py
@@ -5,12 +5,11 @@
 
 from spack.package import *
 
-
 class Pace(CMakePackage):
     """interatomic Potentials in Atomic Cluster Expansion (PACE)
 
     This library is required to build the ML-PACE module
-    in LAMMPS. 
+    in LAMMPS.
 
     The library was developed at the Interdisciplinary Centre
     for Advanced Materials Simulation (ICAMS), Ruhr University Bochum
@@ -21,12 +20,17 @@ class Pace(CMakePackage):
 
     maintainers("hjjvandam")
 
-    homepage = "https://www.icams.de/institute/departments-groups/atomistic-modelling-and-simulation/"
+    homepage = (
+        "https://www.icams.de/institute/departments-groups/atomistic-modelling-and-simulation/"
+    )
     git = "https://github.com/ICAMS/lammps-user-pace.git"
     url = "https://github.com/ICAMS/lammps-user-pace/archive/e2d941286da81a286adf60cc9ddd2c794dc4a6f2.tar.gz"
 
     # See https://spdx.org/licenses/ for a list. Upon manually verifying
     # the license, set checked_by to your Github username.
     license("GPL-2.0-or-later", checked_by="hjjvandam")
-    version("2024.11.9", sha256="fac15d79d981353627305f7759c92b64022740f0a8b8c47dc7125967f4650c70",
-            url="https://github.com/ICAMS/lammps-user-pace/archive/e2d941286da81a286adf60cc9ddd2c794dc4a6f2.tar.gz")
+    version(
+        "2024.11.9",
+        sha256="fac15d79d981353627305f7759c92b64022740f0a8b8c47dc7125967f4650c70",
+        url="https://github.com/ICAMS/lammps-user-pace/archive/e2d941286da81a286adf60cc9ddd2c794dc4a6f2.tar.gz"
+    )
