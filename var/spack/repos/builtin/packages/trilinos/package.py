3077cbd1057cf6b68ff5104c57934df2bddc0799
Andreas Fink <finkandreas@web.de>
2024-11-20 17:38:27 +0100

    also cuda/rocm arch moved to when block

--- var/spack/repos/builtin/packages/trilinos/package.py
+++ var/spack/repos/builtin/packages/trilinos/package.py
@@ -420,15 +420,12 @@ class Trilinos(CMakePackage, CudaPackage, ROCmPackage):
         depends_on("kokkos-kernels@4.2.01", when="@15.1:15")
         depends_on("kokkos-kernels@4.1.00", when="@14.4:15.0")
 
-    for a in CudaPackage.cuda_arch_values:
-        arch_str = f"+cuda cuda_arch={a}"
-        kokkos_spec = f"kokkos {arch_str}"
-        depends_on(kokkos_spec, when=f"@14.4: +kokkos {arch_str}")
-
-    for a in ROCmPackage.amdgpu_targets:
-        arch_str = f"+rocm amdgpu_target={a}"
-        kokkos_spec = f"kokkos {arch_str}"
-        depends_on(kokkos_spec, when=f"@14.4: +kokkos {arch_str}")
+        for a in CudaPackage.cuda_arch_values:
+            arch_str = f"+cuda cuda_arch={a}"
+            depends_on(f"kokkos{arch_str}", when=arch_str)
+        for a in ROCmPackage.amdgpu_targets:
+            arch_str = f"+rocm amdgpu_target={a}"
+            depends_on(f"kokkos{arch_str}", when=arch_str)
 
     depends_on("adios2", when="+adios2")
     depends_on("binder@1.3:", when="@15: +python", type="build")
