211de13ad3bdcc8fef5ad25bb5e1325c3503cf79
Rocco Meli <r.meli@bluemail.ch>
2024-11-21 09:07:14 +0100

    gnina: add v1.3

--- var/spack/repos/builtin/packages/gnina/package.py
+++ var/spack/repos/builtin/packages/gnina/package.py
@@ -18,15 +18,18 @@ class Gnina(CMakePackage, CudaPackage):
 
     license("Apache-2.0")
 
+    version("master", branch="master")
+    version("1.3", sha256="79630705190576669c9613cc3e1e63f1122cba4e363e73c3a0bd7e21f76f443f")
     version("1.1", sha256="114570b0f84a545ce0fea5b2da87bc116c134cef64bf90e6e58e8f84e175a0fa")
     version("1.0.3", sha256="4274429f38293d79c7d22ab08aca91109e327e9ce3f682cd329a8f9c6ef429da")
-
-    depends_on("cxx", type="build")  # generated
 
     variant("cudnn", default=True, description="Build with cuDNN")
     variant("gninavis", default=False, description="Build gninavis")
 
-    _boost = "boost" + "".join(
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+
+    _boost_extensions = " " + "".join(
         [
             "+atomic",
             "+chrono",
@@ -53,11 +56,8 @@ class Gnina(CMakePackage, CudaPackage):
     )
 
     depends_on("zlib-api")
-    depends_on(_boost)
-    depends_on("glog")
+    depends_on("boost@:1.79" + _boost_extensions)
     depends_on("protobuf@:3.21.12")
-    depends_on("hdf5+cxx+hl~mpi")
-    depends_on("openblas~fortran")
 
     depends_on("libmolgrid")
 
@@ -68,13 +68,31 @@ class Gnina(CMakePackage, CudaPackage):
     depends_on("py-numpy", type="build")
     depends_on("py-pytest", type="build")
 
-    depends_on("cuda@11")
+    depends_on("cuda@11", when="@:1.1")
+    depends_on("cuda@12", when="@1.3:")
     depends_on("cudnn", when="+cudnn")
 
+    depends_on("cmake@3.27:", when="@1.3:")  # CMake policy CMP0146 introduced in 3.27
+    depends_on("jsoncpp", when="@1.3")
+    depends_on("py-torch", when="@1.3")
+
+    depends_on("glog@:0.6", when="@:1.1")
+    depends_on("hdf5+cxx+hl~mpi", when="@:1.1")
+    depends_on("openblas~fortran", when="@:1.1")
+
+    patch("PR280-v1.3.patch", when="@1.3")
+    patch("PR282-v1.3.patch", when="@1.3")
+
     def cmake_args(self):
-        args = ["-DBLAS=Open"]  # Use OpenBLAS instead of Atlas' BLAS
+        args = []
+
+        if self.spec.satisfies("@:1.1"):
+            argg.append("-DBLAS=Open")  # Use OpenBLAS instead of Atlas' BLAS
 
         if self.spec.satisfies("+gninavis"):
             args.append(f"-DRDKIT_INCLUDE_DIR={self.spec['rdkit'].prefix.include.rdkit}")
+
+        if self.spec.satisfies("@1.3:"):
+            args.append("-DGNINA_FORCE_EXTERNAL_LIBS=ON")
 
         return args
