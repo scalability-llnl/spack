c89ca16d670621ec8ca15e3e59a03969eb29f427
Adam J. Stewart <ajstewart426@gmail.com>
2024-11-24 12:11:43 +0100

    Replace dev version with stable release

--- var/spack/repos/builtin/packages/glib/package.py
+++ var/spack/repos/builtin/packages/glib/package.py
@@ -30,7 +30,8 @@ class Glib(MesonPackage, AutotoolsPackage):
 
     license("LGPL-2.1-or-later")
 
-    version("2.83.0", sha256="a07d9e1a57a4279c5ece71c26dc44eea12bd518ea9ff695d53e722997032b614")
+    # Even minor versions are stable, odd minor versions are development, only add even numbers
+    version("2.82.2", sha256="ab45f5a323048b1659ee0fbda5cecd94b099ab3e4b9abf26ae06aeb3e781fd63")
     version("2.78.3", sha256="609801dd373796e515972bf95fc0b2daa44545481ee2f465c4f204d224b2bc21")
     version("2.78.0", sha256="44eaab8b720877ce303c5540b657b126f12dc94972d9880b52959f43fb537b30")
     version("2.76.6", sha256="1136ae6987dcbb64e0be3197a80190520f7acab81e2bfb937dc85c11c8aa9f04")
