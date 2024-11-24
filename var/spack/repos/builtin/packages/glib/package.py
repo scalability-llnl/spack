808cd0682574fb9f5ec843719dbc50ffd1f3df53
Adam J. Stewart <ajstewart426@gmail.com>
2024-11-24 10:55:36 +0100

    glib: add v2.83.0

--- var/spack/repos/builtin/packages/glib/package.py
+++ var/spack/repos/builtin/packages/glib/package.py
@@ -30,6 +30,7 @@ class Glib(MesonPackage, AutotoolsPackage):
 
     license("LGPL-2.1-or-later")
 
+    version("2.83.0", sha256="a07d9e1a57a4279c5ece71c26dc44eea12bd518ea9ff695d53e722997032b614")
     version("2.78.3", sha256="609801dd373796e515972bf95fc0b2daa44545481ee2f465c4f204d224b2bc21")
     version("2.78.0", sha256="44eaab8b720877ce303c5540b657b126f12dc94972d9880b52959f43fb537b30")
     version("2.76.6", sha256="1136ae6987dcbb64e0be3197a80190520f7acab81e2bfb937dc85c11c8aa9f04")
@@ -139,6 +140,8 @@ class Glib(MesonPackage, AutotoolsPackage):
     )
 
     with when("build_system=meson"):
+        depends_on("meson@1.4:", when="@2.83:", type="build")
+        depends_on("meson@1.2:", when="@2.79:", type="build")
         depends_on("meson@0.60.0:", when="@2.73:", type="build")
         depends_on("meson@0.52.0:", when="@2.71:2.72", type="build")
         depends_on("meson@0.49.2:", when="@2.61.2:2.70", type="build")
@@ -149,8 +152,9 @@ class Glib(MesonPackage, AutotoolsPackage):
     depends_on("zlib-api")
     depends_on("gettext")
     depends_on("perl", type=("build", "run"))
+    depends_on("python", type=("build", "run"), when="@2.53.4:")
     # Uses distutils in gio/gdbus-2.0/codegen/utils.py
-    depends_on("python@:3.11", type=("build", "run"), when="@2.53.4:")
+    depends_on("python@:3.11", type=("build", "run"), when="@2.53.4:2.78")
     depends_on("pcre2", when="@2.73.2:")
     depends_on("pcre2@10.34:", when="@2.74:")
     depends_on("pcre+utf", when="@2.48:2.73.1")
