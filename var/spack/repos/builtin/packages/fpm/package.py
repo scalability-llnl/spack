968b1eddd611be817042078f200d5e3c148cc2e3
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-11-27 12:10:25 +0100

    fpm: add a dependency on c, cxx, and fortran
    Extracted from #45189
    
    Build failure: https://gitlab.spack.io/spack/spack/-/jobs/13871774

--- var/spack/repos/builtin/packages/fpm/package.py
+++ var/spack/repos/builtin/packages/fpm/package.py
@@ -41,6 +41,10 @@ class Fpm(Package):
     depends_on("curl", type="build")
     depends_on("git@1.8.5:", type="build")
 
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+    depends_on("fortran", type="build")
+
     def setup_build_environment(self, env):
         if self.spec.satisfies("@0.4.0"):
             env.set("FPM_C_COMPILER", self.compiler.cc)
