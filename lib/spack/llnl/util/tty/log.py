0b76a34340443d329e97f1d67d03e244bb96c198
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-12-13 15:13:04 +0100

    Remove encoding= from tty.log

--- lib/spack/llnl/util/tty/log.py
+++ lib/spack/llnl/util/tty/log.py
@@ -762,9 +762,7 @@ class winlog:
             self.reader = open(self.logfile, mode="rb+")
 
             # Dup stdout so we can still write to it after redirection
-            self.echo_writer = open(  # pylint: disable=unspecified-encoding
-                os.dup(sys.stdout.fileno()), "w"
-            )
+            self.echo_writer = open(os.dup(sys.stdout.fileno()), "w", encoding=sys.stdout.encoding)
             # Redirect stdout and stderr to write to logfile
             self.stderr.redirect_stream(self.writer.fileno())
             self.stdout.redirect_stream(self.writer.fileno())
