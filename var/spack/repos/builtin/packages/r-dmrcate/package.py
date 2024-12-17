b0cafdb8a75de6c1a02714c685f90813fc9ac072
Mosè Giordano <m.giordano@ucl.ac.uk>
2024-12-17 13:31:02 +0100

    r-dmrcate: add new versions

--- var/spack/repos/builtin/packages/r-dmrcate/package.py
+++ var/spack/repos/builtin/packages/r-dmrcate/package.py
@@ -17,6 +17,9 @@ class RDmrcate(RPackage):
 
     bioc = "DMRcate"
 
+    version("3.2.0", commit="1f46517bcdea269a2c9726d1156495d6dd172067")
+    version("3.0.0", commit="9cd77dedef76528990487b931ae0cc314423c3b9")
+    version("2.16.0", commit="9e42d8c8eb26dbd2d36a70bf32be322062ecc850")
     version("2.14.0", commit="6e7bae0917001e7664f01c3f8d261f9fe28c2f4d")
     version("2.12.0", commit="560dd5067b05715631739d0fb58ef9cebdbf7078")
     version("2.10.0", commit="81e83701da5c55ac83d0e0b5e640a9d431f09551")
