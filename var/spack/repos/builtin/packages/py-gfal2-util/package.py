033919c96c05b5f19f9c1dd93716c2baa3b93f75
Wouter Deconinck <wdconinc@gmail.com>
2024-12-17 10:45:18 -0600

    py-gfal2-util: new package

---
+++ var/spack/repos/builtin/packages/py-gfal2-util/package.py
@@ -0,0 +1,51 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class PyGfal2Util(PythonPackage):
+    """FIXME: Put a proper description of your package here."""
+
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "https://www.example.com"
+
+    # FIXME: ensure the package is not available through PyPI. If it is,
+    # re-run `spack create --force` with the PyPI URL.
+    url = "https://github.com/cern-fts/gfal2-util/archive/refs/tags/v1.5.3.tar.gz"
+
+    # FIXME: Add a list of GitHub accounts to
+    # notify when the package is updated.
+    # maintainers("github_user1", "github_user2")
+
+    # FIXME: Add the SPDX identifier of the project's license below.
+    # See https://spdx.org/licenses/ for a list. Upon manually verifying
+    # the license, set checked_by to your Github username.
+    license("UNKNOWN", checked_by="github_user1")
+
+    version("1.5.3", sha256="8a5ba5596fe9c63a7a1810b7af1514fdd727eee6d848480a684f2d9134951551")
+
+    # FIXME: Only add the python/pip/wheel dependencies if you need specific versions
+    # or need to change the dependency type. Generic python/pip/wheel dependencies are
+    # added implicity by the PythonPackage base class.
+    # depends_on("python@2.X:2.Y,3.Z:", type=("build", "run"))
+    # depends_on("py-pip@X.Y:", type="build")
+    # depends_on("py-wheel@X.Y:", type="build")
+
+    # FIXME: Add a build backend, usually defined in pyproject.toml. If no such file
+    # exists, use setuptools.
+    # depends_on("py-setuptools", type="build")
+    # depends_on("py-hatchling", type="build")
+    # depends_on("py-flit-core", type="build")
+    # depends_on("py-poetry-core", type="build")
+
+    # FIXME: Add additional dependencies if required.
+    # depends_on("py-foo", type=("build", "run"))
+
+    def config_settings(self, spec, prefix):
+        # FIXME: Add configuration settings to be passed to the build backend
+        # FIXME: If not needed, delete this function
+        settings = {}
+        return settings
