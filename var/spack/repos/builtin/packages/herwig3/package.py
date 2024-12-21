e689ddc78512d3a6d028566b767e3f3b2efd3a67
Wouter Deconinck <wdconinc@gmail.com>
2024-12-21 17:38:38 -0600

    herwig3: fix conflicts ^evtgen

--- var/spack/repos/builtin/packages/herwig3/package.py
+++ var/spack/repos/builtin/packages/herwig3/package.py
@@ -36,7 +36,7 @@ class Herwig3(AutotoolsPackage):
     depends_on("thepeg@2.3.0", when="@7.3.0")
     depends_on("evtgen")
     conflicts(
-        "evtgen ~photos ~pythia8 ~sherpa ~tauola",
+        "^evtgen ~photos ~pythia8 ~sherpa ~tauola",
         msg="At least one external EvtGen component required",
     )
 
