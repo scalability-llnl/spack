4aa2bbe0e67f297466a35cc53b7a09aa51efdeb8
Adam J. Stewart <ajstewart426@gmail.com>
2024-12-28 23:16:58 -0500

    python 3.12 still not supported

--- var/spack/repos/builtin/packages/py-unfoldnd/package.py
+++ var/spack/repos/builtin/packages/py-unfoldnd/package.py
@@ -15,6 +15,9 @@ class PyUnfoldnd(PythonPackage):
 
     version("0.2.2", sha256="e8fdffeb68bc1b393ddc1b1c87056e0e4616db992e95c7dbc3dc90d564599397")
 
+    # https://github.com/f-dangel/unfoldNd/pull/37#issuecomment-2564606819
+    depends_on("python@:3.11", type=("build", "run"))
+
     with default_args(type="build"):
         depends_on("py-setuptools@38.3:")
         depends_on("py-setuptools-scm")
