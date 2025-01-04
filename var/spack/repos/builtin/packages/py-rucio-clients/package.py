ce65e04e8b69b75240bd22a05986d7b98aa3a19e
wdconinc <wdconinc@users.noreply.github.com>
2025-01-04 22:47:07 +0000

    [@spackbot] updating style on behalf of wdconinc

--- var/spack/repos/builtin/packages/py-rucio-clients/package.py
+++ var/spack/repos/builtin/packages/py-rucio-clients/package.py
@@ -15,8 +15,12 @@ class PyRucioClients(PythonPackage):
 
     license("Apache-2.0", checked_by="wdconinc")
 
-    version("36.0.0.post2", sha256="48ac2e3217aac9aaa70133cbfff991560bbeb162165bcf3dd3425967c8a2f816")
-    version("36.0.0.post1", sha256="141aafdde66080d36708dedc9f06a72c55918ee1d138b8cd2f5d2fe43cbc504f")
+    version(
+        "36.0.0.post2", sha256="48ac2e3217aac9aaa70133cbfff991560bbeb162165bcf3dd3425967c8a2f816"
+    )
+    version(
+        "36.0.0.post1", sha256="141aafdde66080d36708dedc9f06a72c55918ee1d138b8cd2f5d2fe43cbc504f"
+    )
     version("36.0.0", sha256="80fbf3b2ec63c13ac1ce430d769fcc526a5f742ba3960ecc64560e0d4cd465b5")
     version("35.6.0", sha256="3c77dea0ce95b7649211da08cee7e93fa9ecb1a6c91bbe750b76b4c576a8b0dd")
     version("35.5.0", sha256="bc79602193e271f66c3fdb43e7abda7903026795d6f3c5d71afb5e52250f8d92")
