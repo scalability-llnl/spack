511addadfe47089ead3cbd81c30e52691d09983e
Niklas Bölter <niklas.boelter@gwdg.de>
2025-01-07 11:07:52 +0100

    Adding dependency constraints and types

--- var/spack/repos/builtin/packages/py-nvitop/package.py
+++ var/spack/repos/builtin/packages/py-nvitop/package.py
@@ -14,13 +14,17 @@ class PyNvitop(PythonPackage):
     homepage = "https://nvitop.readthedocs.io/"
     pypi = "nvitop/nvitop-1.4.0.tar.gz"
 
-    license("Apache-2.0")
+    license("Apache-2.0", checked_by="nboelte")
 
     version("1.4.0", sha256="92f313e9bd89fe1a9d54054e92f490f34331f1b7847a89ddaffd6a7fde1437bb")
 
-    depends_on("py-nvidia-ml-py")
-    depends_on("py-psutil")
-    depends_on("py-cachetools")
-    depends_on("py-termcolor")
-    depends_on("py-setuptools")
+    depends_on("py-nvidia-ml-py@11.450.51:12.561", type=("build", "run"))
+    depends_on("py-psutil@5.6.6:", type=("build", "run"))
+    depends_on("py-cachetools@1.0.1:", type=("build", "run"))
+    depends_on("py-termcolor@1.0.0:", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
+    depends_on("py-colorama@0.4:", when="platform=windows", type=("build", "run"))
 
+    # Windows support would require this extra package to be available in spack.
+    #depends_on("py-windows-curses@2.2.0:", when="platform=windows", type=("build", "run"))
+    conflicts("platform=windows")
