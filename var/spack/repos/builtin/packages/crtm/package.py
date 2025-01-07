5da4e8945ce6e5d537878f3bb116d1af8bf550ec
Alex Richert <alexander.richert@noaa.gov>
2025-01-07 12:14:47 -0800

    Update package.py

--- var/spack/repos/builtin/packages/crtm/package.py
+++ var/spack/repos/builtin/packages/crtm/package.py
@@ -50,7 +50,10 @@ class Crtm(CMakePackage):
 
     license("CC0-1.0")
 
-    version("v3.1.1-build1", sha256="1ed49e594da5d3769cbaa52cc7fc19c1bb0325ee6324f6057227c31e2d95ca67")
+    version(
+        "v3.1.1-build1",
+        sha256="1ed49e594da5d3769cbaa52cc7fc19c1bb0325ee6324f6057227c31e2d95ca67",
+    )
     version(
         "v3.1.0-skylabv8",
         sha256="a475c8a444072aef1e8c2babba3d12f13ab0fb6c7ecab88edad57130839e29ff",
@@ -83,10 +86,9 @@ class Crtm(CMakePackage):
     def url_for_version(self, version):
         if self.spec.satisfies("@v3") or version >= Version("3.0.0"):
             fmtversion = str(version).replace("-build", "+build")
-            url_fmt = f"https://github.com/JCSDA/CRTMv3/archive/refs/tags/{fmtversion}.tar.gz"
+            return f"https://github.com/JCSDA/CRTMv3/archive/refs/tags/{fmtversion}.tar.gz"
         else:
-            url_fmt = f"https://github.com/JCSDA/crtm/archive/refs/tags/{version}.tar.gz"
-        return url_fmt
+            return f"https://github.com/JCSDA/crtm/archive/refs/tags/{version}.tar.gz"
 
     # https://github.com/JCSDA/spack-stack/issues/1088
     patch("v3.1.0-skylabv8.installprefix.patch", when="@v3.1.0-skylabv8")
