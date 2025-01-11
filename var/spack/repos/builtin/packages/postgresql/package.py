086ef7b2edbbfdc061904cc115f0e1f82078347e
Wouter Deconinck <wdconinc@gmail.com>
2025-01-11 08:40:26 -0600

    postgresql: depends on pkgconfig when icu or xml2

--- var/spack/repos/builtin/packages/postgresql/package.py
+++ var/spack/repos/builtin/packages/postgresql/package.py
@@ -66,6 +66,7 @@ class Postgresql(AutotoolsPackage):
     variant("icu", default=True, description="Build with ICU support.", when="@16:")
 
     depends_on("icu4c", when="@16: +icu")
+    depends_on("pkgconfig", when="@16: +icu", type="build")
     depends_on("readline", when="lineedit=readline")
     depends_on("libedit", when="lineedit=libedit")
     depends_on("openssl")
@@ -73,6 +74,7 @@ class Postgresql(AutotoolsPackage):
     depends_on("perl+opcode", when="+perl")
     depends_on("python", when="+python")
     depends_on("libxml2", when="+xml")
+    depends_on("pkgconfig", when="+xml", type="build")
 
     @property
     def command(self):
