b86bfcc695e04e2d9e08268e0fca8e9db2ae1cec
Mathieu Taillefumier <mathieu.taillefumier@free.fr>
2025-01-13 16:59:37 +0100

    Fix formating

--- var/spack/repos/builtin/packages/trexio/package.py
+++ var/spack/repos/builtin/packages/trexio/package.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack.build_systems import cmake, autotools
+from spack.build_systems import autotools, cmake
 from spack.package import *
 
 
