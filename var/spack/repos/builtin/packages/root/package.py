e76da6bdf5f2550e37af7fff7f7f39c116709ec0
Xuefeng Ding (IHEP) <dingxf@ihep.ac.cn>
2025-01-13 11:05:20 +0800

    fix #48446

--- var/spack/repos/builtin/packages/root/package.py
+++ var/spack/repos/builtin/packages/root/package.py
@@ -95,7 +95,6 @@ class Root(CMakePackage):
     depends_on("c", type="build")
     depends_on("cxx", type="build")
     depends_on("fortran", type="build", when="+fortran")
-    extends("python", when="+python")
 
     # ###################### Patches ##########################
 
@@ -844,6 +843,7 @@ class Root(CMakePackage):
         # the following vars are copied from thisroot.sh; silence a cppyy warning
         env.set("CLING_STANDARD_PCH", "none")
         env.set("CPPYY_API_PATH", "none")
+        env.set("CPPYY_BACKEND_LIBRARY",self.prefix.lib.root.libcppyy_backend)
         if "+rpath" not in self.spec:
             env.prepend_path(self.root_library_path, self.prefix.lib.root)
 
