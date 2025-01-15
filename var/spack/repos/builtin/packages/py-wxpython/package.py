3a91e78c097dd0dce76b0b3094cd8a69b0d2d09d
Brendan Moloney <moloney.brendan@gmail.com>
2025-01-15 15:13:51 -0800

    BF+ENH: Add wxwidgets 3.2.6 and py-wxpython 4.2.2
    Improves compat with newer Python (>3.9) and numpy.
    
    Fix error during configure step (even on at least some older
    versions) by including 'pkgconfig' as a build dep so gtk+ is found.

--- var/spack/repos/builtin/packages/py-wxpython/package.py
+++ var/spack/repos/builtin/packages/py-wxpython/package.py
@@ -11,6 +11,7 @@ class PyWxpython(PythonPackage):
     homepage = "https://www.wxpython.org/"
     pypi = "wxPython/wxPython-4.0.6.tar.gz"
 
+    version("4.2.2", sha256="5dbcb0650f67fdc2c5965795a255ffaa3d7b09fb149aa8da2d0d9aa44e38e2ba")
     version("4.1.1", sha256="00e5e3180ac7f2852f342ad341d57c44e7e4326de0b550b9a5c4a8361b6c3528")
     version("4.0.6", sha256="35cc8ae9dd5246e2c9861bb796026bbcb9fb083e4d49650f776622171ecdab37")
 
@@ -18,10 +19,12 @@ class PyWxpython(PythonPackage):
     depends_on("cxx", type="build")  # generated
 
     depends_on("wxwidgets +gui")
+    depends_on("wxwidgets@3.2.6 +gui", when="@4.2.2")
 
     # Needed for the build.py script
     depends_on("py-setuptools", type="build")
     depends_on("py-pathlib2", type="build")
+    depends_on("pkgconfig", type="build")
 
     # Needed at runtime
     depends_on("py-numpy", type=("build", "run"))
