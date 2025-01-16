2241ad726d555bd3f90d3f040a43f3b4460156a1
Jacob Healy <jnhealy@sandia.gov>
2025-01-16 11:02:44 -0700

    fix failing tests

--- lib/spack/spack/test/concretization/core.py
+++ lib/spack/spack/test/concretization/core.py
@@ -3241,7 +3241,7 @@ def test_spec_unification(unify, mutable_config, mock_packages):
 
 
 def test_concretize_dev_path_with_at_symbol_in_env(tmpdir, mock_packages):
-    spec_like = "zlib@2.0"
+    spec_like = "trivial-smoke-test@1.0"
     spec = Spec(spec_like)
     add = SpackCommand("add")
     develop = SpackCommand("develop")
