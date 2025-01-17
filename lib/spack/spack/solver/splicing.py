18fb5dc0b00fbe70f9e14db739b04d7423bf06d6
John Gouwar <jgouwar@gmail.com>
2025-01-17 08:25:14 -0500

    Apply suggestions from code review
    Co-authored-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- lib/spack/spack/solver/splicing.py
+++ lib/spack/spack/solver/splicing.py
@@ -9,28 +9,13 @@ from spack.spec import Spec
 from spack.traverse import by_dag_hash, traverse_nodes
 
 
-class Splice:
-    """A class representing a single splice. These will always be indexed in a
-    dict by the parent spec that they are being spliced into. Note: this should
-    be a dataclass, but Python 3.6 dose not support dataclasses.
-
-    splice_spec (Spec): the spec being spliced into a parent
-    child_name (str): the name of the child that `splice_spec` is replacing
-    child_hash (str): the hash of the child that `splice_spec` is replacing
-    """
-
-    def __init__(self, splice_spec: Spec, child_name: str, child_hash: str):
-        self.splice_spec = splice_spec
-        self.child_name = child_name
-        self.child_hash = child_hash
-
-    def __repr__(self) -> str:
-        output = (
-            f"Splice spec {self.splice_spec}, "
-            f"replacing child {self.child_name} "
-            f"with hash {self.child_hash}"
-        )
-        return output
+class Splice(NamedTuple):
+    #: The spec being spliced into a parent
+    splice_spec: Spec
+    #: The name of the child that splice spec is replacing
+    child_name: str
+    #: The hash of the child that `splice_spec` is replacing
+    child_hash: str
 
 
 def _resolve_collected_splices(
@@ -56,16 +41,13 @@ def _resolve_collected_splices(
         else:
             return 0
 
-    already_resolved: Dict[Spec, Spec] = {}
-    # create a mapping from dag hash to an integer representing position in reverse topo order.
     splice_order = sorted(specs, key=cmp_to_key(splice_cmp))
     reverse_topo_order = reversed(
-        list(traverse_nodes(splice_order, order="topo", key=by_dag_hash))
+        [x for x in traverse_nodes(splice_order, order="topo", key=by_dag_hash) if x in specs]
     )
-    splice_lookup = {spec.dag_hash(): index for index, spec in enumerate(reverse_topo_order)}
-    # iterate over specs, children before parents
 
-    for spec in sorted(specs, key=lambda x: splice_lookup[x.dag_hash()]):
+    already_resolved: Dict[Spec, Spec] = {}
+    for spec in reverse_topo_order:
         immediate = splices.get(spec, [])
         if not immediate and not any(
             edge.spec in already_resolved for edge in spec.edges_to_dependencies()
