4c99da554243abf00ced29dea567bd43656280e7
Tuomas Koskela <t.koskela@ucl.ac.uk>
2025-01-17 12:48:35 +0000

    Fix style

--- var/spack/repos/builtin/packages/purify/package.py
+++ var/spack/repos/builtin/packages/purify/package.py
@@ -29,7 +29,12 @@ class Purify(CMakePackage):
     variant("benchmarks", default=False, description="Build benchmarks")
     variant("docs", default=False, description="Enable multithreading with OpenMP")
     variant("coverage", default=False, description="Enable code coverage")
-    variant("onnxrt", when="@5.0.0:", default=False, description="Build with Tensorflow support using onnx in SOPT")
+    variant(
+        "onnxrt",
+        when="@5.0.0:",
+        default=False,
+        description="Build with Tensorflow support using onnx in SOPT",
+    )
 
     depends_on("cmake@3")
     depends_on("eigen@3.4:3")
