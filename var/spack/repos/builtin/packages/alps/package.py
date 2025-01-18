86f6960ecc51a28ee8d3e5d1158fc57d4148158b
Sinan81 <Sinan@world>
2025-01-17 18:21:19 -0800

    first attempt at providing boost source via resource

--- var/spack/repos/builtin/packages/alps/package.py
+++ var/spack/repos/builtin/packages/alps/package.py
@@ -22,6 +22,7 @@ class Alps(CMakePackage):
 
     license("BSL-1.0", checked_by="github_user1")
 
+    version("2.3.3-beta.6", sha256="eb0c8115b034dd7a9dd585d277c4f86904ba374cdbdd130545aca1c432583b68")
     version("2.3.3-beta.5", sha256="b01c537ea74b57f82dbd97e27ec62e9dce57f9ea05ba9e98f53c6ad370c5f317")
 
     depends_on("c", type="build")
@@ -43,10 +44,20 @@ class Alps(CMakePackage):
 
     extends("python")
 
+    resource(
+    name="boost_source_files",
+    url="https://downloads.sourceforge.net/project/boost/boost/1.82.0/boost_1_82_0.tar.bz2",
+    sha256="a6e1ab9b0860e6a2881dd7b21fe9f737a095e5f33a3a874afc6a345228597ee6",
+    #when='^boost@1.82.0', # this didn't work for some reason.
+    placement="boost_source_files",
+    )
+
+
     def cmake_args(self):
         args = []
         # Don't use Boost_ROOT_DIR option
         args.append("-DCMAKE_CXX_FLAGS={0}".format(self.compiler.cxx14_flag + " -fpermissive"))
+        args.append("-DBoost_SRC_DIR={0}".format(join_path(self.stage.source_path,'boost_source_files')))
         return args
 
     @run_after('install')
