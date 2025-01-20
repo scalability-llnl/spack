fc93c5e5b542e95aa4d9414909b0b6d9074a4f02
Juan Miguel Carceller <22276694+jmcarcell@users.noreply.github.com>
2025-01-20 22:06:44 +0100

    Update var/spack/repos/builtin/packages/pandorasdk/package.py
    Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>

--- var/spack/repos/builtin/packages/pandorasdk/package.py
+++ var/spack/repos/builtin/packages/pandorasdk/package.py
@@ -38,9 +38,8 @@ class Pandorasdk(CMakePackage):
     def cmake_args(self):
         args = [
             self.define("CMAKE_MODULE_PATH", self.spec["pandorapfa"].prefix.cmakemodules),
-            self.define(
-                "CMAKE_CXX_FLAGS", f"-Wno-error -std=c++{self.spec.variants['cxxstd'].value}"
-            ),
+            self.define("CMAKE_CXX_FLAGS", "-Wno-error"),
+            self.define("CMAKE_CXX_STANDARD", self.spec.variants['cxxstd'].value),
         ]
         return args
 
