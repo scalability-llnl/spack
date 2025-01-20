c1ecf4fad7cc6f4dbdaa9d167d37d7dd126f7a96
Jacob Healy <jnhealy@sandia.gov>
2025-01-20 13:15:27 -0700

    Change string formatting to avoid multiline f string

--- lib/spack/spack/solver/asp.py
+++ lib/spack/spack/solver/asp.py
@@ -2710,8 +2710,8 @@ class SpackSolverSetup:
         if env:
             dev_specs = tuple(
                 spack.spec.Spec(info["spec"]).constrained(
-                    "dev_path="
-                    f'"{spack.util.path.canonicalize_path(info["path"], default_wd=env.path)}"'
+                    'dev_path="%s"'
+                    % spack.util.path.canonicalize_path(info["path"], default_wd=env.path)
                 )
                 for name, info in env.dev_specs.items()
             )
