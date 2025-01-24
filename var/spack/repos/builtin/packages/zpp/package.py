7208f1cfa048d301dbcdac21deda5759a143ea16
Etienne Malaboeuf <eti.malaboeuf@gmail.com>
2025-01-24 16:23:18 +0100

    Typo

--- var/spack/repos/builtin/packages/zpp/package.py
+++ var/spack/repos/builtin/packages/zpp/package.py
@@ -6,6 +6,7 @@
 from spack import *
 from spack.package import *
 
+
 class Zpp(PythonPackage):
     """ZPP, the Z Pre-Processor. ZPP transforms bash in a pre-processor for F90
     source files. It offers a set of functions specifically tailored to build
@@ -30,6 +31,7 @@ class Zpp(PythonPackage):
     depends_on("py-setuptools", type=("build", "run"))
 
     def url_for_version(self, version):
+        fixed = ""
         if version > Version("1.0.7") and version <= Version("1.0.9"):
             fixed = "-fixed"
         url = "https://files.pythonhosted.org/packages/source/z/zpp/zpp-{version}{fixed}.tar.gz"
