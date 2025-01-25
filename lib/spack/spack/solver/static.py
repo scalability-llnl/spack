c54d0e4951d9cb458820baba50659612d17e45df
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2025-01-25 16:26:48 +0100

    Inlined the call to package_base.possible_dependencies

--- lib/spack/spack/solver/static.py
+++ lib/spack/spack/solver/static.py
@@ -5,7 +5,7 @@
 from typing import Set, Tuple
 
 import spack.deptypes as dt
-import spack.package_base
+import spack.repo
 import spack.spec
 
 
@@ -14,9 +14,31 @@ class PossibleDependenciesAnalyzer:
         self, *specs: spack.spec.Spec, allowed_deps: dt.DepFlag
     ) -> Tuple[Set[str], Set[str]]:
         virtuals: Set[str] = set()
-        real_packages = set(
-            spack.package_base.possible_dependencies(
-                *specs, virtuals=virtuals, depflag=allowed_deps
+
+        packages = []
+        for current_spec in specs:
+            if isinstance(current_spec, str):
+                current_spec = spack.spec.Spec(current_spec)
+
+            if spack.repo.PATH.is_virtual(current_spec.name):
+                packages.extend(
+                    [
+                        (current_spec, p.package_class)
+                        for p in spack.repo.PATH.providers_for(current_spec.name)
+                    ]
+                )
+                continue
+            packages.append((current_spec, current_spec.package_class))
+
+        visited = {}
+        for input_spec, pkg in packages:
+            pkg.possible_dependencies(
+                visited=visited,
+                transitive=True,
+                expand_virtuals=True,
+                depflag=allowed_deps,
+                virtuals=virtuals,
             )
-        )
+
+        real_packages = set(visited)
         return real_packages, virtuals
