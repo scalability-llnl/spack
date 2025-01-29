fc73dce8cb0b1bd5f20c2a8ad0bf0b9bec548f4a
adaell <155860187+adaell@users.noreply.github.com>
2025-01-29 22:45:52 +0000

    Update package.py
    More changes to the style.

--- var/spack/repos/builtin/packages/maxima/package.py
+++ var/spack/repos/builtin/packages/maxima/package.py
@@ -9,9 +9,6 @@ class Maxima(AutotoolsPackage):
 
     """
     Maxima is a system for the manipulation of symbolic and numerical expressions, including differentiation, integration, Taylor series, Laplace transforms, ordinary differential equations, systems of linear equations, polynomials, sets, lists, vectors, matrices and tensors. Maxima yields high precision numerical results by using exact fractions, arbitrary-precision integers and variable-precision floating-point numbers. Maxima can plot functions and data in two and three dimensions.
-
-    The Maxima source code can be compiled on many systems, including Windows, Linux, and MacOS X. The source code for all systems and precompiled binaries for Windows and Linux are available at the SourceForge file manager.
-
     """
 
     homepage = "https://maxima.sourceforge.io/"
