074ed3ec242f849c2d2d1488016c6d59803be55f
adaell <155860187+adaell@users.noreply.github.com>
2025-01-29 22:42:40 +0000

    Update package.py
    Fixed style error.

--- var/spack/repos/builtin/packages/maxima/package.py
+++ var/spack/repos/builtin/packages/maxima/package.py
@@ -5,25 +5,6 @@
 
 from spack.package import *
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install maxima
-#
-# You can edit this file again by typing:
-#
-#     spack edit maxima
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-from spack.package import *
-
-
 class Maxima(AutotoolsPackage):
 
     """
@@ -43,7 +24,6 @@ class Maxima(AutotoolsPackage):
     )
 
     depends_on("sbcl")
-    #    depends_on("texinfo")
 
     def configure_args(self):
         args = [f"--disable-build-docs"]
