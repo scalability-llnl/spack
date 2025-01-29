c98bc53bc2acf4b3c0e251e3efd9ce75b0d63c05
Kayla Butler <butler59@llnl.gov>
2025-01-29 10:57:34 -0800

    Update append, prepend, and remove The current regex expression do not work Lowercased the argument names Added helper funcs

--- share/spack/shell_commands.sh
+++ share/spack/shell_commands.sh
@@ -1,107 +1,145 @@
 #!/bin/sh
 
-
-_value_in_varname () {
-    VARNAME="$1"
-    VALUE="$2"
-    SEP="$3"
-
-    if eval "[ -n \"\${${VARNAME}}\" ]"; then
-        if eval "[ \"\${${VARNAME}}\" == \"$VALUE\" ] ||
-            [ \"\${${VARNAME}}\" == ^\"$VALUE$SEP\"* ] ||
-            [ \"\${${VARNAME}}\" == *\"$SEP$VALUE$SEP\"* ] ||
-            [ \"\${${VARNAME}}\" == *\"$SEP$VALUE\" ]"; then
-            return 0
-        fi
+# separator_exists sep
+#
+# Fails if separator argument was not supplied
+separator_exists() {
+    if [ -z "$1" ]; then
+        echo "Missing argument: separator"
+        exit 1
     fi
-    return 1
 }
 
-# _spack_env_set VARNAME VALUE
+# _value_not_in_varname varname value sep
 #
-# Set the VARNAME variable to VALUE.
+# Return whether the variable value is found in varname
+_value_not_in_varname () {
+    varname="$1"
+    value="$2"
+    sep="$3"
+
+    eval "var=\"\${${varname}}\""
+
+    echo "here $var"
+    if eval "[ \"yes another one\" =~ \"*another*\" ]"; then
+        echo "it's here"
+    else
+        echo "not here"
+    fi
+
+    if eval "[ \"\${${varname}}\" == \"$value\" ] ||
+        [ \"\${${varname}}\" == ^\"$value$sep\"* ] ||
+        [ \"\${${varname}}\" == *\"$sep$value$sep\"* ] ||
+        [ \"\${${varname}}\" == *\"$sep$value\" ]"; then
+            echo "1"
+            false
+    fi
+    echo "0"
+    true
+}
+
+# _spack_env_varname_is_empty varname
+#
+# Return whether the variable varname is unset or set to the empty string.
+_spack_env_varname_is_empty() {
+    eval "test -z \"\${$1}\""
+}
+
+# _spack_env_set varname value
+#
+# Set the varname variable to value.
 _spack_env_set() {
-    VARNAME="$1"
-    VALUE="$2"
+    varname="$1"
+    value="$2"
 
-    export $VARNAME="$VALUE"
+    export $varname="$value"
 }
 
-# _spack_env_unset VARNAME
+# _spack_env_unset varname
 #
-# Unset VARNAME in the environment.
+# Unset varname in the environment.
 _spack_env_unset() {
-    VARNAME="$1"
+    varname="$1"
 
-    unset "$VARNAME"
+    unset "$varname"
 }
 
-# _spack_env_append VARNAME VALUE SEP
+# _spack_env_append varname value sep
 #
-# Append VALUE to the flag list in variable VARNAME.
-# The list in VARNAME is separated by the SEP character.
+# Append value to the flag list in variable varname.
+# The list in varname is separated by the sep character.
 _spack_env_append() {
-    VARNAME="$1"
-    VALUE="$2"
-    SEP="$3"
+    varname="$1"
+    value="$2"
+    sep="$3"
 
-    if eval "[ -n \"\${${VARNAME}}\" ]"; then
-        if [ "$(_value_in_varname $VARNAME $VALUE)" == 1 ] ; then
-            eval "export $VARNAME=\${${VARNAME}}$SEP$VALUE"
-        fi
+    $(separator_exists $sep)
+
+    if _spack_env_varname_is_empty "$varname"; then
+        eval "$varname=\"\${value}\""
     else
-        export $VARNAME="$VALUE"
+        echo "in else"
+        echo "$(_value_not_in_varname $varname $value $sep)"
+        if [ "$(_value_not_in_varname $varname $value $sep)" ] ; then
+            echo "setting"
+            eval "export $varname=\${${varname}}$sep$value"
+        fi
     fi
 }
 
-# _spack_env_prepend VARNAME VALUE SEP
+# _spack_env_prepend varname value sep
 #
-# Prepend VALUE to the flag list in variable VARNAME.
-# The list in VARNAME is separated by the SEP character.
+# Prepend value to the flag list in variable varname.
+# The list in varname is separated by the sep character.
 _spack_env_prepend() { # if not exporting then use lowercase
-    VARNAME="$1"
-    VALUE="$2"
-    SEP="$3" # how to set if no $3
+    varname="$1"
+    value="$2"
+    sep="$3"
 
-   if eval "[ -n \"\${${VARNAME}}\" ]"; then
-        if [ "$(_value_in_varname $VARNAME $VALUE)" == 1 ] ; then
-            eval "export $VARNAME=$VALUE$SEP\${${VARNAME}}"
+    $(separator_exists $sep)
+
+   if _spack_env_varname_is_empty "$varname"; then
+        eval "$varname=\"\${value}\""
+    else
+        if [ "$(_value_not_in_varname $varname $value $sep)" ] ; then
+            eval "export $varname=$value$sep\${${varname}}"
         fi
-    else
-        export $VARNAME="$VALUE"
     fi
 }
 
-# _spack_env_remove VARNAME VALUE SEP
+# _spack_env_remove varname value sep
 #
-# Remove VALUE from the flag list in variable VARNAME.
-# The list in VARNAME is separated by the SEP character.
+# Remove value from the flag list in variable varname.
+# The list in varname is separated by the sep character.
 _spack_env_remove() { # look into sed
-    VARNAME="$1"
-    VALUE="$2"
-    SEP="$3"
+    varname="$1"
+    value="$2"
+    sep="$3"
 
-    VARNAME_VALUE=\${${VARNAME}}
+    $(separator_exists $sep)
 
+    eval "varname_value=\"\${${varname}}\""
 
-    eval "echo \"${VARNAME_VALUE[@]/other*/}\""
-    eval echo "${VARNAME/\$VALUE}"
-
-    if [ $VARNAME == *"$VALUE"* ]; then
-        ${${VARNAME}} = ""
-
+    # if varname's only value is $value
+    if eval "[ \"$varname_value\" == \"$value\" ]"; then
+        export $varname=""
     else
-        echo "Find a way to remove the VALUE"
-        # ${${VARNAME}} = ${VARNAME/${VALUE}/""}
+        # if value is at the beginning or middle  of the string
+        if [ "$varname_value" == ^"$value$sep"* ] ||
+            [ "$varname_value" == *"$sep$value$sep"* ]; then
+            eval "export $varname=\"${varname_value[@]/$value$sep/}\""
+        # if value is at the end of the string
+        elif [ "$varname_value" == *"$sep$value" ]; then
+            eval "export $varname=\"${varname_value[@]/$sep$value/}\""
     fi
 }
 
-# _spack_env_prune_duplicate VARNAME SEP
+# _spack_env_prune_duplicate varname sep
 #
 # Remove duplicate elements from the list in variable
-# VARNAME, preserving precedence.
+# varname, preserving precedence.
 #
-# The list in VARNAME is separated by the SEP character.
+# The list in varname is separated by the sep character.
 _spack_env_prune_duplicates() {
     echo
 }
