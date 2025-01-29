0615636b3cc3e737c404c7d1139ee1e10f0b3ac8
adaell <155860187+adaell@users.noreply.github.com>
2025-01-29 22:38:00 +0000

    [@spackbot] updating style on behalf of adaell

--- var/spack/repos/builtin/packages/maxima/package.py
+++ var/spack/repos/builtin/packages/maxima/package.py
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+from spack.package import *
+
 # ----------------------------------------------------------------------------
 # If you submit this package back to Spack as a pull request,
 # please first remove this boilerplate and all FIXME comments.
@@ -19,30 +21,32 @@
 #
 # See the Spack documentation for more information on packaging.
 # ----------------------------------------------------------------------------
-
 from spack.package import *
+
 
 class Maxima(AutotoolsPackage):
 
     """
-        Maxima is a system for the manipulation of symbolic and numerical expressions, including differentiation, integration, Taylor series, Laplace transforms, ordinary differential equations, systems of linear equations, polynomials, sets, lists, vectors, matrices and tensors. Maxima yields high precision numerical results by using exact fractions, arbitrary-precision integers and variable-precision floating-point numbers. Maxima can plot functions and data in two and three dimensions.
+    Maxima is a system for the manipulation of symbolic and numerical expressions, including differentiation, integration, Taylor series, Laplace transforms, ordinary differential equations, systems of linear equations, polynomials, sets, lists, vectors, matrices and tensors. Maxima yields high precision numerical results by using exact fractions, arbitrary-precision integers and variable-precision floating-point numbers. Maxima can plot functions and data in two and three dimensions.
 
-        The Maxima source code can be compiled on many systems, including Windows, Linux, and MacOS X. The source code for all systems and precompiled binaries for Windows and Linux are available at the SourceForge file manager.
+    The Maxima source code can be compiled on many systems, including Windows, Linux, and MacOS X. The source code for all systems and precompiled binaries for Windows and Linux are available at the SourceForge file manager.
 
     """
+
     homepage = "https://maxima.sourceforge.io/"
     url = "https://sourceforge.net/projects/maxima/files/Maxima-source/5.47.0-source/maxima-5.47.0.tar.gz/download"
     maintainers("adaell")
     license("BSD-3-Clause", checked_by="adaell")
-    version("5.47.0", sha512="953b98336eb086069edaa917981372450ce165dedd7a7b39b181d3f2d8b089551eea943e0084148eaa3ded395dcba9135509d3d11d9132ab7a3ad8bb800d3a11")
+    version(
+        "5.47.0",
+        sha512="953b98336eb086069edaa917981372450ce165dedd7a7b39b181d3f2d8b089551eea943e0084148eaa3ded395dcba9135509d3d11d9132ab7a3ad8bb800d3a11",
+    )
 
     depends_on("sbcl")
-#    depends_on("texinfo")
+    #    depends_on("texinfo")
 
     def configure_args(self):
-        args = [
-                f"--disable-build-docs"
-        ]
+        args = [f"--disable-build-docs"]
 
         return args
 
