5b531fb1dc5e90de95b657496317b1b0c8d6a063
adaell <155860187+adaell@users.noreply.github.com>
2025-01-29 22:53:55 +0000

    Update package.py
    style

--- var/spack/repos/builtin/packages/maxima/package.py
+++ var/spack/repos/builtin/packages/maxima/package.py
@@ -9,11 +9,13 @@ from spack.package import *
 class Maxima(AutotoolsPackage):
 
     """
-    Maxima is a system for the manipulation of symbolic and numerical expressions, including differentiation, integration, 
-    Taylor series, Laplace transforms, ordinary differential equations, systems of linear equations, polynomials, sets, 
-    lists, vectors, matrices and tensors. Maxima yields high precision numerical results by using exact fractions, 
-    arbitrary-precision integers and variable-precision floating-point numbers. Maxima can plot functions and data in two
-    and three dimensions.
+    Maxima is a system for the manipulation of symbolic and numerical expressions,
+    including differentiation, integration, Taylor series, Laplace transforms, 
+    ordinary differential equations, systems of linear equations, polynomials, sets, 
+    lists, vectors, matrices and tensors. Maxima yields high precision numerical 
+    results by using exact fractions, arbitrary-precision integers and 
+    variable-precision floating-point numbers. Maxima can plot functions and 
+    data in two and three dimensions.
     """
 
     homepage = "https://maxima.sourceforge.io/"
