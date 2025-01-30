8153a4404ab9a48ca5e141e3d6ca7ed61f746de5
adaell <155860187+adaell@users.noreply.github.com>
2025-01-30 00:56:52 +0000

    Update package.py
    shortened description

--- var/spack/repos/builtin/packages/maxima/package.py
+++ var/spack/repos/builtin/packages/maxima/package.py
@@ -8,13 +8,8 @@ from spack.package import *
 
 class Maxima(AutotoolsPackage):
     """
-    Maxima is a system for the manipulation of symbolic and numerical expressions,
-    including differentiation, integration, Taylor series, Laplace transforms,
-    ordinary differential equations, systems of linear equations, polynomials, sets,
-    lists, vectors, matrices and tensors. Maxima yields high precision numerical
-    results by using exact fractions, arbitrary-precision integers and
-    variable-precision floating-point numbers. Maxima can plot functions and
-    data in two and three dimensions.
+    Maxima is a fairly complete computer algebra system written in Lisp 
+    with an emphasis on symbolic computation.
     """
 
     homepage = "https://maxima.sourceforge.io/"
