3a9ae04f67ed37b50d0d5b0e31e1f1c3994db29c
Rocco Meli <r.meli@bluemail.ch>
2025-02-03 10:50:33 +0100

    patch pugixml

--- var/spack/repos/builtin/packages/sirius/package.py
+++ var/spack/repos/builtin/packages/sirius/package.py
@@ -165,6 +165,11 @@ class Sirius(CMakePackage, CudaPackage, ROCmPackage):
         conflicts("^spla@:1.5~openmp", when="+openmp")
 
     patch("libxc7.patch", when="@7.6:")
+    patch(
+        "https://patch-diff.githubusercontent.com/raw/electronic-structure/SIRIUS/pull/1050.patch",
+        sha256="ce222c0c06dac0af8f26afc523cd21191177b2947c64964ebec26fb46a980c4b",
+        when="@7.6.0:7.6.1 +pugixml",
+    )
 
     depends_on("nlcglib", when="+nlcglib")
     depends_on("nlcglib+rocm", when="+nlcglib+rocm")
