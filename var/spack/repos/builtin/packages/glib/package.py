2bd1a66c66e0fb98dba4c27eb653762717277ac8
Harmen Stoppels <me@harmenstoppels.nl>
2025-02-03 10:07:10 +0100

    glib: remove deprecated versions

--- var/spack/repos/builtin/packages/glib/package.py
+++ var/spack/repos/builtin/packages/glib/package.py
@@ -4,24 +4,22 @@
 
 import os
 
-import spack.build_systems.autotools
 import spack.build_systems.meson
 from spack.package import *
 from spack.util.environment import is_system_path
 
 
-class Glib(MesonPackage, AutotoolsPackage):
-    """GLib provides the core application building blocks for
-    libraries and applications written in C.
+class Glib(MesonPackage):
+    """GLib provides the core application building blocks for libraries and applications written
+    in C.
 
-    The GLib package contains a low-level libraries useful for
-    providing data structure handling for C, portability wrappers
-    and interfaces for such runtime functionality as an event loop,
+    The GLib package contains a low-level libraries useful for providing data structure handling
+    for C, portability wrappers and interfaces for such runtime functionality as an event loop,
     threads, dynamic loading and an object system.
     """
 
     homepage = "https://developer.gnome.org/glib/"
-    url = "https://download.gnome.org/sources/glib/2.53/glib-2.53.1.tar.xz"
+    url = "https://download.gnome.org/sources/glib/2.82/glib-2.82.2.tar.xz"
     list_url = "https://download.gnome.org/sources/glib"
     list_depth = 1
 
@@ -67,64 +65,9 @@ class Glib(MesonPackage, AutotoolsPackage):
     version("2.62.6", sha256="104fa26fbefae8024ff898330c671ec23ad075c1c0bce45c325c6d5657d58b9c")
     version("2.60.7", sha256="8b12c0af569afd3b71200556ad751bad4cf4bf7bc4b5f880638459a42ca86310")
     version("2.58.3", sha256="8f43c31767e88a25da72b52a40f3301fefc49a665b56dc10ee7cc9565cbe7481")
-    version(
-        "2.56.4",
-        sha256="27f703d125efb07f8a743666b580df0b4095c59fc8750e8890132c91d437504c",
-        deprecated=True,
-    )
-    version(
-        "2.56.3",
-        sha256="a9a4c5b4c81b6c75bc140bdf5e32120ef3ce841b7413214ecf5f987acec74cb2",
-        deprecated=True,
-    )
-    version(
-        "2.56.2",
-        sha256="d64abd16813501c956c4e123ae79f47f1b58de573df9fdd3b0795f1e2c1aa789",
-        deprecated=True,
-    )
-    version(
-        "2.56.1",
-        sha256="40ef3f44f2c651c7a31aedee44259809b6f03d3d20be44545cd7d177221c0b8d",
-        deprecated=True,
-    )
-    version(
-        "2.56.0",
-        sha256="ecef6e17e97b8d9150d0e8a4b3edee1ac37331213b8a2a87a083deea408a0fc7",
-        deprecated=True,
-    )
-    version(
-        "2.55.1",
-        sha256="0cbb3d31c9d181bbcc97cba3d9dbe3250f75e2da25e5f7c8bf5a993fe54baf6a",
-        deprecated=True,
-    )
-    version(
-        "2.53.1",
-        sha256="c8740f1d1a138086eede889b596a511fddda180646ab2f1d98aed4fdb6be7f72",
-        deprecated=True,
-    )
-    version(
-        "2.49.7",
-        sha256="0fd13406ca31d6f654c3be620e0adaaa4f9fb788e164e265e33edf4b21e64ef6",
-        deprecated=True,
-    )
-    version(
-        "2.49.4",
-        sha256="9e914f9d7ebb88f99f234a7633368a7c1133ea21b5cac9db2a33bc25f7a0e0d1",
-        deprecated=True,
-    )
-    version(
-        "2.48.1",
-        sha256="74411bff489cb2a3527bac743a51018841a56a4d896cc1e0d0d54f8166a14612",
-        deprecated=True,
-    )
-    version(
-        "2.42.1",
-        sha256="8f3f0865280e45b8ce840e176ef83bcfd511148918cc8d39df2ee89b67dcf89a",
-        deprecated=True,
-    )
 
-    depends_on("c", type="build")  # generated
-    depends_on("cxx", type="build")  # generated
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
 
     variant("libmount", default=False, description="Build with libmount support")
     variant(
@@ -133,45 +76,30 @@ class Glib(MesonPackage, AutotoolsPackage):
         description="Enable tracing support",
     )
 
-    build_system(
-        conditional("meson", when="@2.58:"),
-        conditional("autotools", when="@:2.57"),
-        default="meson",
-    )
+    with default_args(type="build"):
+        depends_on("meson@1.4:", when="@2.83:")
+        depends_on("meson@1.2:", when="@2.79:")
+        depends_on("meson@0.60.0:", when="@2.73:")
+        depends_on("meson@0.52.0:", when="@2.71:2.72")
+        depends_on("meson@0.49.2:", when="@2.61.2:2.70")
+        depends_on("meson@0.48.0:", when="@:2.61.1")
+        depends_on("pkgconfig", type="build")
 
-    with when("build_system=meson"):
-        depends_on("meson@1.4:", when="@2.83:", type="build")
-        depends_on("meson@1.2:", when="@2.79:", type="build")
-        depends_on("meson@0.60.0:", when="@2.73:", type="build")
-        depends_on("meson@0.52.0:", when="@2.71:2.72", type="build")
-        depends_on("meson@0.49.2:", when="@2.61.2:2.70", type="build")
-        depends_on("meson@0.48.0:", when="@:2.61.1", type="build")
-
-    depends_on("pkgconfig", type="build")
     depends_on("libffi")
     depends_on("zlib-api")
     depends_on("gettext")
     depends_on("perl", type=("build", "run"))
-    depends_on("python", type=("build", "run"), when="@2.53.4:")
+    depends_on("python", type=("build", "run"))
     # Uses distutils in gio/gdbus-2.0/codegen/utils.py
-    depends_on("python@:3.11", type=("build", "run"), when="@2.53.4:2.78")
+    depends_on("python@:3.11", type=("build", "run"), when="@:2.78")
     depends_on("pcre2", when="@2.73.2:")
     depends_on("pcre2@10.34:", when="@2.74:")
-    depends_on("pcre+utf", when="@2.48:2.73.1")
+    depends_on("pcre+utf", when="@:2.73.1")
     depends_on("uuid", when="+libmount")
     depends_on("util-linux", when="+libmount")
     depends_on("iconv")
     depends_on("elf")  # bin/gresource
 
-    # The following patch is needed for gcc-6.1
-    patch("g_date_strftime.patch", when="@2.42.1")
-    # Clang doesn't seem to acknowledge the pragma lines to disable the -Werror
-    # around a legitimate usage.
-    patch("no-Werror=format-security.patch", when="@:2.57")
-    # Patch to prevent compiler errors in kernels older than 2.6.35
-    patch("old-kernels.patch", when="@2.56.0:2.56.1 os=rhel6")
-    patch("old-kernels.patch", when="@2.56.0:2.56.1 os=centos6")
-    patch("old-kernels.patch", when="@2.56.0:2.56.1 os=scientific6")
     # fix multiple definition error in gio tests for 2.76.1
     patch(
         "https://gitlab.gnome.org/GNOME/glib/-/merge_requests/3368.diff",
@@ -181,7 +109,7 @@ class Glib(MesonPackage, AutotoolsPackage):
 
     # glib prefers the libc version of gettext, which breaks the build if the
     # external version is also found.
-    patch("meson-gettext.patch", when="@2.58:2.64")
+    patch("meson-gettext.patch", when="@:2.64")
     patch("meson-gettext-2.66.patch", when="@2.66:2.68,2.72")
     patch("meson-gettext-2.70.patch", when="@2.70")
 
@@ -193,9 +121,7 @@ class Glib(MesonPackage, AutotoolsPackage):
     )
 
     def url_for_version(self, version):
-        """Handle glib's version-based custom URLs."""
-        url = "https://download.gnome.org/sources/glib"
-        return url + "/%s/glib-%s.tar.xz" % (version.up_to(2), version)
+        return f"https://download.gnome.org/sources/glib/{version.up_to(2)}/glib-{version}.tar.xz"
 
     def patch(self):
         """A few glib tests have external dependencies / try to access the X server"""
@@ -214,7 +140,8 @@ class Glib(MesonPackage, AutotoolsPackage):
         return find_libraries(["libglib*"], root=self.prefix, recursive=True)
 
 
-class AnyBuilder(BaseBuilder):
+class MesonBuilder(spack.build_systems.meson.MesonBuilder):
+
     @property
     def dtrace_copy_path(self):
         return join_path(self.stage.source_path, "dtrace-copy")
@@ -293,8 +220,6 @@ class AnyBuilder(BaseBuilder):
             myfile = join_path(self.spec["glib"].libs.directories[0], "pkgconfig", "glib-2.0.pc")
             filter_file(pattern, repl, myfile, backup=False)
 
-
-class MesonBuilder(AnyBuilder, spack.build_systems.meson.MesonBuilder):
     def meson_args(self):
         args = []
         if self.spec.satisfies("@2.63.5:"):
@@ -333,47 +258,4 @@ class MesonBuilder(AnyBuilder, spack.build_systems.meson.MesonBuilder):
                     args.append("-Diconv=gnu")
             else:
                 args.append("-Diconv=libc")
-        return args
-
-
-class AutotoolsBuilder(AnyBuilder, spack.build_systems.autotools.AutotoolsBuilder):
-    def configure_args(self):
-        args = []
-        if self.spec.satisfies("+libmount"):
-            args.append("--enable-libmount")
-        else:
-            args.append("--disable-libmount")
-        if self.spec.satisfies("@2.53.4:"):
-            args.append(
-                "--with-python={0}".format(os.path.basename(self.spec["python"].command.path))
-            )
-        if self.spec["iconv"].name == "libiconv":
-            args.append("--with-libiconv=gnu")
-        else:
-            args.append("--with-libiconv=maybe")
-        if self.spec.satisfies("@2.56:"):
-            for value in ("dtrace", "systemtap"):
-                if ("tracing=" + value) in self.spec:
-                    args.append("--enable-" + value)
-                else:
-                    args.append("--disable-" + value)
-        else:
-            if self.spec.satisfies("tracing=dtrace") or self.spec.satisfies("tracing=systemtap"):
-                args.append("--enable-tracing")
-            else:
-                args.append("--disable-tracing")
-        # SELinux is not available in Spack, so glib should not use it.
-        args.append("--disable-selinux")
-        # glib should not use the globally installed gtk-doc. Otherwise,
-        # gtk-doc can fail with Python errors such as "ImportError: No module
-        # named site". This is due to the fact that Spack sets PYTHONHOME,
-        # which can confuse the global Python installation used by gtk-doc.
-        args.append("--disable-gtk-doc-html")
-        # glib uses gtk-doc even though it should be disabled if it can find
-        # its binaries. Override the checks to use the true binary.
-        true = which("true")
-        args.append("GTKDOC_CHECK={0}".format(true))
-        args.append("GTKDOC_CHECK_PATH={0}".format(true))
-        args.append("GTKDOC_MKPDF={0}".format(true))
-        args.append("GTKDOC_REBASE={0}".format(true))
         return args
