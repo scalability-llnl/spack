031ae799dd269b28f6109311d130e5d00a2bd864
Chris Marsh <chrismarsh.c2@gmail.com>
2025-02-03 10:22:37 -0600

    fix when

--- var/spack/repos/builtin/packages/py-geojson/package.py
+++ var/spack/repos/builtin/packages/py-geojson/package.py
@@ -18,6 +18,6 @@ class PyGeojson(PythonPackage):
     version("3.2.0", sha256="b860baba1e8c6f71f8f5f6e3949a694daccf40820fa8f138b3f712bd85804903")
     version("3.1.0", sha256="58a7fa40727ea058efc28b0e9ff0099eadf6d0965e04690830208d3ef571adac")
 
-    depends_on("python@3.7:3.12", when="3.1.0")
-    depends_on("python@3.7:3.13", when="3.2.0")
+    depends_on("python@3.7:3.12", when="@3.1.0")
+    depends_on("python@3.7:3.13", when="@3.2.0")
     depends_on("py-setuptools", type="build")
