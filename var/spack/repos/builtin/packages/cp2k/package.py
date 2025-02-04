93776ab59d49cc11bf661ab0e71a04509e245425
Mathieu Taillefumier <mathieu.taillefumier@free.fr>
2025-02-04 14:57:33 +0100

    [package] Add the possibility to turn on/off GPU accelation for GRID, PW, and DBM modules in cp2k
    - All three modules are enabled on nvidia hardware. GRID and DBM modules are enabled with rocm, pw is off by default (some issues with more than one mpi rank per gpu)
    - Add the possibility to compile the grid hip backend on nvidia hardware.

--- var/spack/repos/builtin/packages/cp2k/package.py
+++ var/spack/repos/builtin/packages/cp2k/package.py
@@ -119,7 +119,26 @@ class Cp2k(MakefilePackage, CMakePackage, CudaPackage, ROCmPackage):
     variant("dftd4", when="@2024.2:", default=False, description="Enable DFT-D4 support")
     variant("mpi_f08", default=False, description="Use MPI F08 module")
     variant("smeagol", default=False, description="Enable libsmeagol support", when="@2025.2:")
-
+    variant(
+        "pw_gpu",
+        default=True,
+        description="Enable FFT calculations on GPU (off by default for now)",
+        when="@2025.2: +cuda",
+    )
+    variant("grid_gpu", default=True, description="Enable grid gpu backend", when="@2025.2:")
+    variant("dbm_gpu", default=True, description="Eanble DBM gpu backend", when="@2025.2:")
+    variant(
+        "pw_gpu",
+        default=False,
+        description="Enable FFT calculations on GPU",
+        when="@2025.2: +rocm",
+    )
+    variant(
+        "hip_backend",
+        default=False,
+        description="Enable HIP backend on Nvidia GPU",
+        when="@2025.2:+cuda",
+    )
     variant(
         "enable_regtests",
         default=False,
@@ -147,6 +166,11 @@ class Cp2k(MakefilePackage, CMakePackage, CudaPackage, ROCmPackage):
             when="@:7",  # req in CP2K v8+
             description="Use CUBLAS for general matrix operations in DBCSR",
         )
+
+    depends_on("hipcc", when="+hip_backend+cuda")
+    depends_on("hip+cuda", when="+hip_backend+cuda")
+    depends_on("hipfft+cuda", when="+hip_backend+cuda")
+    depends_on("hipblas+cuda", when="+hip_backend+cuda")
 
     HFX_LMAX_RANGE = range(4, 8)
 
@@ -983,10 +1007,15 @@ class CMakeBuilder(cmake.CMakeBuilder):
                 raise InstallError("CP2K supports only one cuda_arch at a time.")
             else:
                 gpu_ver = GPU_MAP[spec.variants["cuda_arch"].value[0]]
-                args += [
-                    self.define("CP2K_USE_ACCEL", "CUDA"),
-                    self.define("CP2K_WITH_GPU", gpu_ver),
-                ]
+                if spec.satisfies("+hip_backend"):
+                    args += [
+                        self.define("CP2K_USE_ACCEL", "HIP"),
+                        self.define("CMAKE_HIP_PLATFORM=nvidia"),
+                    ]
+                else:
+                    args += [self.define("CP2K_USE_ACCEL", "CUDA")]
+
+                args += [self.define("CP2K_WITH_GPU", gpu_ver)]
 
         if spec.satisfies("+rocm"):
             if len(spec.variants["amdgpu_target"].value) > 1:
@@ -1018,6 +1047,9 @@ class CMakeBuilder(cmake.CMakeBuilder):
             self.define_from_variant("CP2K_USE_DFTD4", "dftd4"),
             self.define_from_variant("CP2K_USE_MPI_F08", "mpi_f08"),
             self.define_from_variant("CP2K_USE_LIBSMEAGOL", "smeagol"),
+            self.define_from_variant("CP2K_ENABLE_GRID_GPU", "grid_gpu"),
+            self.define_from_variant("CP2K_ENABLE_DBM_GPU", "dbm_gpu"),
+            self.define_from_variant("CP2K_ENABLE_PW_GPU", "pw_gpu"),
         ]
 
         # we force the use elpa openmp threading support. might need to be revisited though
@@ -1027,6 +1059,9 @@ class CMakeBuilder(cmake.CMakeBuilder):
                 ("+elpa +openmp" in spec) or ("^elpa +openmp" in spec),
             )
         ]
+
+        if spec.satisfies("+hip_backend"):
+            args += [self.define_from_variant("-DCMAKE_HIP_PLATFORM=nvidia")]
 
         if "spla" in spec and (spec.satisfies("+cuda") or spec.satisfies("+rocm")):
             args += ["-DCP2K_USE_SPLA_GEMM_OFFLOADING=ON"]
