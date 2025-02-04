9172a9457f7ff707646f9878d506957b346c6a07
Richard Berger <rberger@lanl.gov>
2025-02-03 22:01:49 -0700

    flecsi: correct legion dependency

--- var/spack/repos/builtin/packages/flecsi/package.py
+++ var/spack/repos/builtin/packages/flecsi/package.py
@@ -74,7 +74,7 @@ class Flecsi(CMakePackage, CudaPackage, ROCmPackage):
     requires("^kokkos +cuda_constexpr +cuda_lambda", when="^kokkos +cuda")
     depends_on("kokkos +rocm", when="+kokkos +rocm")
     depends_on("kokkos +openmp", when="+kokkos +openmp")
-    depends_on("legion@cr-20210122", when="backend=legion @2.0:2.2.1")
+    depends_on("legion@cr-20210122", when="backend=legion @2.0:2.1.0")
     depends_on("legion@cr-20230307", when="backend=legion @2.2.0:2.2.1")
     depends_on("legion@24.03.0:", when="backend=legion @2.2.2:")
     depends_on("legion@24.09.0:", when="backend=legion @2.3.1:")
