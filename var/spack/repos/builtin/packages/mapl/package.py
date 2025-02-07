67ac27f0c44226f6a27768495b05081db0b6c223
Matthew Thompson <matthew.thompson@nasa.gov>
2025-02-07 15:38:35 -0500

    Add 2.54.1

--- var/spack/repos/builtin/packages/mapl/package.py
+++ var/spack/repos/builtin/packages/mapl/package.py
@@ -38,7 +38,7 @@ class Mapl(CMakePackage):
     version("develop", branch="develop")
     version("main", branch="main")
 
-    version("2.54.0", sha256="067f5a0509e075c0ac61ddc1d505b2c30000abd5c3e0d048fb3208bbd6b1471b")
+    version("2.54.1", sha256="2430ded45a98989e9100037f54cf22f5a5083e17196514b3667d3003413e49e1")
     version("2.53.1", sha256="8371a75d4d81294eb9d99d66702f8cf62d4bd954cec3e247e1afae621b4e4726")
     version("2.53.0", sha256="68c24e6c0e3340645b1fb685972c96ef80746d5a289572c9883e520680708ebe")
     version("2.52.0", sha256="c30be3a6ed3fca40aea903e10ee51e2fb50b4ef2445fdc959d4871baf3c20585")
