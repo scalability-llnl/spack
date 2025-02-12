88eb1918ab5688217d5f33a527d7beefba293254
John Parent <john.parent@kitware.com>
2025-02-12 18:15:51 -0500

    Make Windows make tools make executables
    Adds a setup method to the msvc compiler class to effect this once compilers as nodes lands

--- var/spack/repos/builtin/packages/msvc/package.py
+++ var/spack/repos/builtin/packages/msvc/package.py
@@ -43,6 +43,13 @@ class Msvc(Package, CompilerPackage):
         spec, extras = super().determine_variants(exes, version_str)
         extras["compilers"]["c"] = extras["compilers"]["cxx"]
         return spec, extras
+    
+    def setup_dependent_package(self, module, dependent_spec):
+        """Populates dependent module with tooling available from VS"""
+        # We want these to resolve to the paths set by MSVC's VCVARs
+        # so no paths
+        module.nmake = MakeExecutable("nmake", jobs=1)
+        module.msbuild = MakeExecutable("msbuild", jobs=1)
 
     @property
     def cc(self):
