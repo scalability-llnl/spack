6a40588352237922ca81c70a49e3b7a4fb4ed9c8
Etienne Malaboeuf <eti.malaboeuf@gmail.com>
2025-02-13 11:02:22 +0100

    Version update

--- var/spack/repos/builtin/packages/pdiplugin-decl-hdf5/package.py
+++ var/spack/repos/builtin/packages/pdiplugin-decl-hdf5/package.py
@@ -20,28 +20,15 @@ class PdipluginDeclHdf5(CMakePackage):
     maintainers("jbigot")
 
     version("develop", branch="main", no_cache=True)
-    version("1.8.0", sha256="5d353bfa64f45ee4715b88bd30330030f79f2020cd6bede0ad9b8f9beddadea9")
-    version("1.7.1", sha256="d67e3a498bfe4491c4e9aeb40015b32481a7902b122f087dcebf05451a3d9ce1")
-    version("1.6.0", sha256="ae45d388c98c5e33d552d5e3216c1f92bf97d5dd01c669107084c1f3202fcd5a")
-    version("1.5.5", sha256="11bf5db61f23107dfd2135e637e9233524855c78104c57288c6af21d02d1ea53")
-    version("1.5.4", sha256="0af1fe9fb85066772a921efdf1f3bb554559066a62eaebce4c9c6afd3b2a5c38")
-    version("1.5.3", sha256="aa2fc692f1352d53cf9444f842144648180183313f5c8e7b799e1cb542f6f1ba")
-    version("1.5.2", sha256="8dc14fe7ab1c7efed31e4c7522bc43e191be5dbb4227ffedcb04902d95a6fad0")
-    version("1.5.1", sha256="3b93d238823fc06e9ec5bb0f9f9d9e1a42fdc0061004df0e898d728aaa93ce23")
-    version("1.5.0", sha256="13cbddbdc728a4e19213395034a147bf4e4eb5518c7c734f621a08311343dd4d")
-    version("1.4.3", sha256="b7f049cae9e6fb0ddba3a458e15d6f1578876663c04b18f62b052f9698750974")
-    version("1.4.2", sha256="b252d5098f3b13b5d883265597f567aa0aa423c95fe4a10554772d485ce37e86")
-    version("1.4.1", sha256="55e17629ca373232a8a2530c4ad83403729c74fbe4fcbcfc32e8128800cb40a4")
-    version("1.4.0", sha256="1c273d39df14f44ccb3e6d0fc36f88c19fff7a3f819e7d6d6c80b5ac9eed033b")
-    version("1.3.1", sha256="a4a1f9d4d6ca6790487edf65e3d7c9f5a5d17fb0bcdf81dfab94efc854fcc387")
-    version("1.3.0", sha256="86947c40f025a09ab228360fa002f7241801f5fb70c75f815210d607cf30b200")
-    version("1.2.2", sha256="b93a9165d4b9f1e09790c9be3c950530537cf9a9dc01210afc77e411939bdf41")
-    version("1.2.1", sha256="0c90294fb2bb9ca5f6b957d9d8f68f3a3039fc256ba92f7e5bbe316768b43037")
-    version("1.2.0", sha256="8d5821d473140ea48036e8f03668bf6295d06f8b7561d464cc1b5748bf8d2aa3")
-    version("1.1.0", sha256="8f8a33e1538afde81bb2bbbc3ba8fe3942f0824672a364d0eb2055a0255e8b0c")
-    version("1.0.1", sha256="c35f6d19cecfc3963c08c8d516386c1cd782fcdbe4e39aa91dd01376d2346cb6")
-    version("1.0.0", sha256="57f5bfd2caa35de144651b0f4db82b2a403997799c258ca3a4e632f8ff2cfc1b")
-    version("0.6.5", sha256="a1100effb62d43556bd5e50d82f51e51710dbafc8d85c5a2e03ba7c168460be9")
+    version("1.8.1", commit="105161d5c93431d674c73ef365dce3eb724b4fcb")
+    version("1.8.0", commit="edce72fc198475bab1541cc0b77a30ad02da91c5")
+    version("1.7.1", commit="7eadb5314eae65fa08010bb0d35a4dcace0ebb48")
+    version("1.6.0", commit="92da1ee7fc77dce6614de49ec10d991b14e7fd02")
+    version("1.5.5", commit="43bd8fdae3bd12a950ff68b09d22cd74923f4eb1")
+    version("1.4.3", commit="0e92542d1df70918712cb5ee378bd6ad1199ba2d")
+    version("1.3.1", commit="6a075a7af3383d1d5a6278017760b6d3dc746517")
+    version("1.2.2", commit="4f7076d6e45ba3feead63a7d710695def52b8380")
+    version("1.1.0", commit="5cd945d28d600d54949f610e4c152a59171b2f39")
 
     variant("benchs", default=False, description="Build benchmarks")
     variant("fortran", default=True, description="Enable Fortran (for tests only)")
@@ -60,6 +47,7 @@ class PdipluginDeclHdf5(CMakePackage):
     depends_on("hdf5 +mpi", type=("build", "link", "run"), when="+mpi")
     depends_on("hdf5@1.8:1 +shared", type=("build", "link", "run"))
     depends_on("pdi@develop", type=("link", "run"), when="@develop")
+    depends_on("pdi@1.8.1", type=("link", "run"), when="@1.8.1")
     depends_on("pdi@1.8.0", type=("link", "run"), when="@1.8.0")
     depends_on("pdi@1.7.1", type=("link", "run"), when="@1.7.1")
     depends_on("pdi@1.6.0", type=("link", "run"), when="@1.6.0")
@@ -85,14 +73,6 @@ class PdipluginDeclHdf5(CMakePackage):
     depends_on("pkgconfig", type=("build"))
 
     root_cmakelists_dir = "plugins/decl_hdf5"
-
-    def url_for_version(self, version):
-        if version <= Version("1.7.1"):
-            return (
-                "https://gitlab.maisondelasimulation.fr/pdidev/pdi/-/archive/"
-                + f"{version}/pdi-{version}.tar.bz2"
-            )
-        return f"https://github.com/pdidev/pdi/archive/refs/tags/{version}.tar.gz"
 
     def cmake_args(self):
         args = [
