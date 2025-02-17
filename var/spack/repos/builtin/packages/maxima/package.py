75500fc4954a07c9eeb4516b03090e34fb265a96
Wouter Deconinck <wdconinc@gmail.com>
2025-02-16 22:15:37 -0600

    maxima: fix spdx license header

--- var/spack/repos/builtin/packages/maxima/package.py
+++ var/spack/repos/builtin/packages/maxima/package.py
@@ -1,5 +1,4 @@
-# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
@@ -14,8 +13,11 @@ class Maxima(AutotoolsPackage):
 
     homepage = "https://maxima.sourceforge.io/"
     url = "https://sourceforge.net/projects/maxima/files/Maxima-source/5.47.0-source/maxima-5.47.0.tar.gz/download"
+
     license("BSD-3-Clause", checked_by="adaell")
+ 
     maintainers("adaell")
+
     version("5.47.0", sha256="9104021b24fd53e8c03a983509cb42e937a925e8c0c85c335d7709a14fd40f7a")
 
     depends_on("sbcl")
