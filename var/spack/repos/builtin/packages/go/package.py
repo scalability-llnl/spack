c1f3019e0154a9362c3367310999176ef05d112b
Robert Maaskant <robert@maaskant.io>
2025-02-19 20:33:31 +0100

    go: add v1.24.0

--- var/spack/repos/builtin/packages/go/package.py
+++ var/spack/repos/builtin/packages/go/package.py
@@ -38,44 +38,44 @@ class Go(Package):
 
     license("BSD-3-Clause")
 
+    version("1.24.0", sha256="d14120614acb29d12bcab72bd689f257eb4be9e0b6f88a8fb7e41ac65f8556e5")
     version("1.23.6", sha256="039c5b04e65279daceee8a6f71e70bd05cf5b801782b6f77c6e19e2ed0511222")
     version("1.23.5", sha256="a6f3f4bbd3e6bdd626f79b668f212fbb5649daf75084fb79b678a0ae4d97423b")
     version("1.23.4", sha256="ad345ac421e90814293a9699cca19dd5238251c3f687980bbcae28495b263531")
     version("1.23.3", sha256="8d6a77332487557c6afa2421131b50f83db4ae3c579c3bc72e670ee1f6968599")
     version("1.23.2", sha256="36930162a93df417d90bd22c6e14daff4705baac2b02418edda671cdfa9cd07f")
     version("1.23.1", sha256="6ee44e298379d146a5e5aa6b1c5b5d5f5d0a3365eabdd70741e6e21340ec3b0d")
-    version("1.22.8", sha256="df12c23ebf19dea0f4bf46a22cbeda4a3eca6f474f318390ce774974278440b8")
-    version("1.22.7", sha256="66432d87d85e0cfac3edffe637d5930fc4ddf5793313fe11e4a0f333023c879f")
-    version("1.22.6", sha256="9e48d99d519882579917d8189c17e98c373ce25abaebb98772e2927088992a51")
-    version("1.22.4", sha256="fed720678e728a7ca30ba8d1ded1caafe27d16028fab0232b8ba8e22008fb784")
 
-    # https://nvd.nist.gov/vuln/detail/CVE-2024-24790
-    # https://nvd.nist.gov/vuln/detail/CVE-2024-24789
-    version(
-        "1.22.2",
-        sha256="374ea82b289ec738e968267cac59c7d5ff180f9492250254784b2044e90df5a9",
-        deprecated=True,
-    )
-    version(
-        "1.22.1",
-        sha256="79c9b91d7f109515a25fc3ecdaad125d67e6bdb54f6d4d98580f46799caea321",
-        deprecated=True,
-    )
-    version(
-        "1.22.0",
-        sha256="4d196c3d41a0d6c1dfc64d04e3cc1f608b0c436bd87b7060ce3e23234e1f4d5c",
-        deprecated=True,
-    )
-    version(
-        "1.21.6",
-        sha256="124926a62e45f78daabbaedb9c011d97633186a33c238ffc1e25320c02046248",
-        deprecated=True,
-    )
-    version(
-        "1.21.5",
-        sha256="285cbbdf4b6e6e62ed58f370f3f6d8c30825d6e56c5853c66d3c23bcdb09db19",
-        deprecated=True,
-    )
+    with default_args(deprecated=True):
+        version(
+            "1.22.8", sha256="df12c23ebf19dea0f4bf46a22cbeda4a3eca6f474f318390ce774974278440b8"
+        )
+        version(
+            "1.22.7", sha256="66432d87d85e0cfac3edffe637d5930fc4ddf5793313fe11e4a0f333023c879f"
+        )
+        version(
+            "1.22.6", sha256="9e48d99d519882579917d8189c17e98c373ce25abaebb98772e2927088992a51"
+        )
+        version(
+            "1.22.4", sha256="fed720678e728a7ca30ba8d1ded1caafe27d16028fab0232b8ba8e22008fb784"
+        )
+        # https://nvd.nist.gov/vuln/detail/CVE-2024-24790
+        # https://nvd.nist.gov/vuln/detail/CVE-2024-24789
+        version(
+            "1.22.2", sha256="374ea82b289ec738e968267cac59c7d5ff180f9492250254784b2044e90df5a9"
+        )
+        version(
+            "1.22.1", sha256="79c9b91d7f109515a25fc3ecdaad125d67e6bdb54f6d4d98580f46799caea321"
+        )
+        version(
+            "1.22.0", sha256="4d196c3d41a0d6c1dfc64d04e3cc1f608b0c436bd87b7060ce3e23234e1f4d5c"
+        )
+        version(
+            "1.21.6", sha256="124926a62e45f78daabbaedb9c011d97633186a33c238ffc1e25320c02046248"
+        )
+        version(
+            "1.21.5", sha256="285cbbdf4b6e6e62ed58f370f3f6d8c30825d6e56c5853c66d3c23bcdb09db19"
+        )
 
     provides("golang")
 
@@ -85,6 +85,7 @@ class Go(Package):
     depends_on("go-or-gccgo-bootstrap", type="build")
     depends_on("go-or-gccgo-bootstrap@1.17.13:", type="build", when="@1.20:")
     depends_on("go-or-gccgo-bootstrap@1.20.6:", type="build", when="@1.22:")
+    depends_on("go-or-gccgo-bootstrap@1.22.6:", type="build", when="@1.24:")
 
     phases = ["build", "install"]
 
