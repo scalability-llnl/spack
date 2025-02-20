765d03f9081ef6b38e42e90f8fe3a86cfde98139
ashim-mahara <48154590+ashim-mahara@users.noreply.github.com>
2025-02-20 18:32:42 +0000

    [@spackbot] updating style on behalf of ashim-mahara

--- var/spack/repos/builtin/packages/py-gurobipy/package.py
+++ var/spack/repos/builtin/packages/py-gurobipy/package.py
@@ -3,9 +3,9 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack.package import *
-
 import platform
+
+from spack.package import *
 
 
 class PyGurobipy(PythonPackage):
@@ -38,61 +38,45 @@ class PyGurobipy(PythonPackage):
     if arch == "x86_64":
         version(
             "12.0.0-py3.12",
-            sha256=(
-                "90bba495efb25cff5a3826158aff7be29637d2e80accc3a89a98cb8630856106"
-            ),
+            sha256=("90bba495efb25cff5a3826158aff7be29637d2e80accc3a89a98cb8630856106"),
             expand=False,
         )
         version(
             "12.0.0-py3.11",
-            sha256=(
-                "a08fb42a5e7cb02cdb993c1381c8b8c5a3baeedcadd56e7288d8458a57b81442"
-            ),
+            sha256=("a08fb42a5e7cb02cdb993c1381c8b8c5a3baeedcadd56e7288d8458a57b81442"),
             expand=False,
-            )
+        )
         version(
             "12.0.0-py3.10",
-            sha256=(
-                "71ea7c653a92377991291a7e81b456fdfd06f8e3fc36b74a54444c2bab5225a8"
-            ),
+            sha256=("71ea7c653a92377991291a7e81b456fdfd06f8e3fc36b74a54444c2bab5225a8"),
             expand=False,
-            )
+        )
         version(
             "12.0.0-py3.9",
-            sha256=(
-                "5e0b0ea5ce2eaf4bd940821860fe37dcc8fd3fe7215ab0d97cceaedfa5eeb15d"
-            ),
+            sha256=("5e0b0ea5ce2eaf4bd940821860fe37dcc8fd3fe7215ab0d97cceaedfa5eeb15d"),
             expand=False,
-            )
+        )
     elif arch == "aarch64":
         version(
             "12.0.0-py3.12",
-            sha256=(
-                "f8287de7535c3b9c97f4aa8716969d70302a972bd09e9d3b6fbd7266ca0eab72"
-            ),
+            sha256=("f8287de7535c3b9c97f4aa8716969d70302a972bd09e9d3b6fbd7266ca0eab72"),
             expand=False,
         )
         version(
             "12.0.0-py3.11",
-            sha256=(
-                "fc3892e3d88d0f8a01da75f12f74023d398ef599a9e1add66ed76313733e30fb"
-            ),
+            sha256=("fc3892e3d88d0f8a01da75f12f74023d398ef599a9e1add66ed76313733e30fb"),
             expand=False,
-            )
+        )
         version(
             "12.0.0-py3.10",
-            sha256=(
-                "d4803e659fefc2102e9dc3d84f3d7f54a3918127c362cbebae6103b7e13849c6"
-            ),
+            sha256=("d4803e659fefc2102e9dc3d84f3d7f54a3918127c362cbebae6103b7e13849c6"),
             expand=False,
-            )
+        )
         version(
             "12.0.0-py3.9",
-            sha256=(
-                "68c0ffe4ac8c4d87662307f96f50f40ddc3867d71c6ebabce4bda52f4688ecbd"
-            ),
+            sha256=("68c0ffe4ac8c4d87662307f96f50f40ddc3867d71c6ebabce4bda52f4688ecbd"),
             expand=False,
-            )
+        )
     else:
         conflicts("target={arch}:", msg="py-gurobi is not available for {arch}")
 
