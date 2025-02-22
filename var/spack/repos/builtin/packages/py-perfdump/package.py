8aa2b6b2bf29b237aa54cd7f5f55154cf4dd7364
Chase Phelps <34648910+chaseleif@users.noreply.github.com>
2025-02-22 22:56:51 +0000

    package.py aktualisieren
    Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>

--- var/spack/repos/builtin/packages/py-perfdump/package.py
+++ var/spack/repos/builtin/packages/py-perfdump/package.py
@@ -18,7 +18,7 @@ class PyPerfdump(CMakePackage):
 
     version("1.1", sha256="d2d96e2bd8ba2616ea4a44233ea240a529788390a5c22d35f9de79a22647370d")
 
-    variant("mpi", default=True, description="Use MPI")
+    variant("mpi", default=False, description="Use MPI")
     variant("hdf5", default=True, description="Enable HDF5 output")
 
     depends_on("cmake@3.15:", type="build")
