c499050a9e555dc07d132a9af23b79b41e6ff7e3
sbstndbs <sebastiendubois4@gmail.com>
2025-02-23 11:42:22 +0100

    Add tags
    	-	Add tags for the last versions of samurai
    	-	All tags are tested and worked properly
    	-	Add maintainers ("gouarin" - the samurai project lead and "sbstndb" - me, working on samurai)
    	-	Add licence

--- var/spack/repos/builtin/packages/samurai/package.py
+++ var/spack/repos/builtin/packages/samurai/package.py
@@ -12,17 +12,26 @@ class Samurai(CMakePackage):
     homepage = "https://github.com/hpc-maths/samurai"
     git = "https://github.com/hpc-maths/samurai.git"
 
+    maintainers("gouarin", "sbstndb")
+
+    license("BSD-3-Clause")
+
     version("master", branch="master")
     version("0.21.1", tag="v0.21.1", commit="29798bd9592e5b80674c49080a242378a61878c3")
+    version("0.20.0", tag="v0.20.0", commit="b894b8aebf992e112e129e3b70ccdfc7c19c6647")
+    version("0.19.0", tag="v0.19.0", commit="b35332c5d719a940b045210abd8e4eb9b69f6b85")
+    version("0.18.0", tag="v0.18.0", commit="483c44d1b28dc59e2004d618b094b4de31e1ef7c")
 
     variant("mpi", default=False, description="Enable MPI support")
     variant("openmp", default=False, description="Enable OpenMP support")
+    # variants for a future release
     # variant("demos", default=False, description="Build Demos")
     # variant("benchmarks", default=False,description="Build benchmarks")
     variant("tests", default=False, description="Build tests")
     variant("check_nan", default=False, description="Check for Nan in computations")
 
     depends_on("xtl@0.7.4")
+    # optional dependency for a future release
     # depends_on("xsimd@11.0.0")
     depends_on("xtensor@0.24.1 ~tbb")
     depends_on("highfive~mpi", when="~mpi")
@@ -31,8 +40,6 @@ class Samurai(CMakePackage):
     depends_on("fmt")
     depends_on("nlohmann-json")
     depends_on("cli11")
-
-    # option in future release
     depends_on("cxxopts")
     depends_on("cgal")
     depends_on("petsc~mpi", when="~mpi")
