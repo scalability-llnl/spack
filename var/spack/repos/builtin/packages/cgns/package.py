fe26c6b6a554e72be8394938a3c3f3347f08951e
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2025-02-24 17:22:46 +0100

    cgns: adjust comment
    Co-authored-by: Wouter Deconinck <wdconinc@gmail.com>

--- var/spack/repos/builtin/packages/cgns/package.py
+++ var/spack/repos/builtin/packages/cgns/package.py
@@ -77,7 +77,7 @@ class Cgns(CMakePackage):
     # copied from https://github.com/CGNS/CGNS/pull/757
     # (adjusted an include from tk-private/generic/tkInt.h to tkInt.h)
     patch("gcc14.patch", when="@:4.4.0 %gcc@14:")
-    # "wrong" include for spack (tk-private) from the patch above made its way into the official version
+    # "wrong" include for spack (tk-private) from the patch above made it into the official version
     patch("cgns-4.5-tk-private.patch", when="@4.5.0")
 
     def cmake_args(self):
